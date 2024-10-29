77c9f978ea4ffe95b76776812b7c80e07fe8669f
noah0504789 <58963724+noah0504789@users.noreply.github.com>
2024-10-29 13:20:26 +0900

    [Silver I] Title: 부분수열의 합, Time: 104 ms, Memory: 14380 KB -BaekjoonHub

--- 백준/Silver/14225. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/14225. 부분수열의 합/부분수열의 합.java
@@ -5,7 +5,7 @@ public class Main {
     private static Set<Integer> set;
     private static BitSet visited;
     private static int[] nums;
-    private static int n, ans;
+    private static int n, ans, max;
 
     public static void main(String... args) throws IOException {
         ans = 1;
@@ -14,35 +14,17 @@ public class Main {
         nums = new int[n];
 
         for (int i = 0; i < n; i++) nums[i] = readInt();
-
-        set = new HashSet<>();
-        visited = new BitSet();
-
-        dfs(0);
-
-        while (set.contains(ans)) ans++;
-
-        System.out.print(ans);
-    }
-
-    private static void dfs(int depth) {
-        if (depth == n) {
-            int result = 0;
-
-            for (int i = 0; i < n; i++) {
-                if (visited.get(i)) continue;
-
-                result += nums[i];
-            }
-
-            set.add(result);
-            return;
+        
+        Arrays.sort(nums);
+        
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            if (sum + 1 < nums[i]) break;
+            
+            sum += nums[i];
         }
 
-        visited.set(depth);
-        dfs(depth+1);
-        visited.clear(depth);
-        dfs(depth+1);
+        System.out.print(sum+1);
     }
 
     public static int readInt() throws IOException {
