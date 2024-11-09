9f5594d8e13066b0f209329321f161746fa4f37b
noah0504789 <58963724+noah0504789@users.noreply.github.com>
2024-11-09 23:42:11 +0900

    [Silver II] Title: 키로거, Time: 960 ms, Memory: 137292 KB -BaekjoonHub

--- 백준/Silver/5397. 키로거/키로거.java
+++ 백준/Silver/5397. 키로거/키로거.java
@@ -3,41 +3,49 @@ import java.util.*;
 
 public class Main {        
     private static BufferedReader br;
-    private static StringBuilder sb, cur;
+    private static StringBuilder sb;
 
+    private static Stack<Character> left, right;
     private static char[] input;
-    private static int tc, idx;
+    private static int tc;
 
     public static void main(String... args) throws IOException {
         br = new BufferedReader(new InputStreamReader(System.in));
         sb = new StringBuilder();
-        cur = new StringBuilder();
 
         tc = Integer.parseInt(br.readLine());
 
+        left = new Stack<>();
+        right = new Stack<>();
+
         for (int i = 0; i < tc; i++) {
-            cur.setLength(0);
-            idx = 0;
+            left.clear();
+            right.clear();
 
             for (char c : br.readLine().toCharArray()) {
-                if (c == '<') {
-                    if (idx == 0) continue;
-
-                    idx--;
-                } else if (c == '>') {
-                    if (idx >= cur.length()) continue;
-
-                    idx++;
-                } else if (c == '-') {
-                    if (idx == 0) continue;
+                if (c == '<') {                    
+                    if (left.isEmpty()) continue;
                     
-                    cur.deleteCharAt(--idx);
+                    right.push(left.pop());
+                } else if (c == '>') {
+                    if (right.isEmpty()) continue;
+                    
+                    left.push(right.pop());
+                } else if (c == '-') {
+                    if (left.isEmpty()) continue;
+                    
+                    left.pop();
                 } else {
-                    cur.insert(idx++, c);
+                    left.push(c);
                 }
             }
 
-            sb.append(cur.toString());
+            while (!right.isEmpty()) left.push(right.pop());
+
+            int size = left.size();
+
+            for (int j = 0; j < size; j++) sb.append(left.get(j));
+
             sb.append("\n");
         }
 
