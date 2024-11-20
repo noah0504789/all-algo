a83c9894c1d076b59bd238f5aa6d4f020b93d0d0
noah0504789 <58963724+noah0504789@users.noreply.github.com>
2024-11-20 19:38:23 +0900

    [Platinum V] Title: 팰린드롬 개수 구하기 (Large), Time: 132 ms, Memory: 18860 KB -BaekjoonHub

---
+++ 백준/Platinum/14517. 팰린드롬 개수 구하기 （Large）/팰린드롬 개수 구하기 （Large）.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {        
+    private static BufferedReader br;
+    private static BufferedWriter bw;
+    
+    private static final int MOD = 10_007;
+    private static int[][] dp;
+    private static char[] input;
+    private static int size;
+    
+    public static void main(String... args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        input = br.readLine().toCharArray();
+        
+        size = input.length;
+        
+        dp = new int[size+1][size+1];
+        
+        for (int i = 0; i < size; i++) {
+            dp[i][i] = 1;
+            
+            if (i+1 < size && input[i] == input[i+1]) dp[i][i+1] = 3;
+            else if (i+1 < size) dp[i][i+1] = 2;
+        }
+        
+        for (int lng = 3; lng <= size; lng++) {
+            for (int start = 0; start < size; start++) {
+                int end = start+lng-1;
+                if (end >= size) break;
+                
+                if (input[start] == input[end]) dp[start][end] = dp[start+1][end] + dp[start][end-1] + 1;
+                else dp[start][end] = dp[start+1][end] + dp[start][end-1] - dp[start+1][end-1] + MOD;
+                
+                dp[start][end] %= MOD;
+            }
+        }
+        
+        bw.write(dp[0][size-1]+"");
+        bw.flush();                
+    }
+}
