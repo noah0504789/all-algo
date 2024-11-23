c95b91e71b5ba883b3124bb1a0f22042cec264c3
noah0504789 <58963724+noah0504789@users.noreply.github.com>
2024-11-23 16:20:17 +0900

    [Gold IV] Title: 동작 그만. 밑장 빼기냐?, Time: 132 ms, Memory: 15120 KB -BaekjoonHub

--- 백준/Gold/20159. 동작 그만． 밑장 빼기냐？/동작 그만． 밑장 빼기냐？.java
+++ 백준/Gold/20159. 동작 그만． 밑장 빼기냐？/동작 그만． 밑장 빼기냐？.java
@@ -4,30 +4,34 @@ import java.util.*;
 public class Main {
     private static BufferedWriter bw;
 
-    private static long[] card1, card2;
-    private static long ans, tmp;
-    private static int n;
+    private static long[] odds, evens;
+    private static long ans, lastEven, odd, even;
+    private static int n, size;
 
     public static void main(String... args) throws IOException {
         bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         n = readInt();
+        size = n / 2;
 
-        card1 = new long[n+1];
-        card2 = new long[n+1];
-        
-        for (int i = 1; i <= n/2; i++) {
-            card1[i] = card1[i-1] + readInt();
-            card2[i] = card2[i-1] + readInt();
+        odds = new long[size+1];
+        evens = new long[size+1];
+
+        for (int i = 1; i <= size; i++) {
+            odds[i] = odds[i-1] + readInt();
+            evens[i] = evens[i-1] + readInt();
         }
-        
-        ans = card1[n/2];
-        
-        for (int i = 0; i < n; i++) {
-            if (i % 2 == 0) tmp = card1[i/2+1] + card2[n/2-1] - card2[i/2]; // 나일 때 밑장빼기
-            else tmp = card1[i/2] + card2[n/2] - card2[i/2];
-            
-            ans = Math.max(ans, tmp);
+
+        ans = odds[size];
+        lastEven = evens[size] - evens[size-1];
+
+        for (int i = 0; i <= size; i++) {
+            odd = odds[i];
+            even = evens[size] - evens[i];
+
+            if (i > 0) even = Math.max(even, evens[size] - evens[i-1] - lastEven);
+
+            ans = Math.max(ans, odd+even);
         }
 
         bw.write(ans+"");
