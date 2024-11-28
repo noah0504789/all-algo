0435a2c6e40f4565e998e99e063c3b2f296d4d37
noah0504789 <58963724+noah0504789@users.noreply.github.com>
2024-11-28 18:11:29 +0900

    [Gold IV] Title: 페르마의 크리스마스 정리, Time: 236 ms, Memory: 33896 KB -BaekjoonHub

---
+++ 백준/Gold/4913. 페르마의 크리스마스 정리/페르마의 크리스마스 정리.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    private static BufferedWriter bw;
+
+    private static final int MAX = 1_000_000;
+    private static boolean[] notPrime;
+    private static long[] acc1, acc2;
+    private static long cnt1, cnt2;
+    private static int l, u;
+
+    public static void main(String... args) throws IOException {
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        notPrime = new boolean[MAX+1];
+        notPrime[0] = notPrime[1] = true;
+
+        for (int i = 2; i <= Math.sqrt(MAX); i++) {
+            if (notPrime[i]) continue;
+
+            for (int j = i*i; j <= MAX; j+=i) notPrime[j] = true;
+        }
+
+        acc1 = new long[MAX+1];
+        acc2 = new long[MAX+1];
+        
+        acc1[2] = acc2[2] = 1;
+        for (int i = 3; i <= MAX; i++) {
+            acc1[i] = acc1[i-1] + (!notPrime[i] ? 1 : 0);
+            acc2[i] = acc2[i-1] + (!notPrime[i] && (i % 4 == 1) ? 1 : 0);
+        }
+
+        while (true) {
+            l = readInt();
+            u = readInt();
+            
+            if (l == -1 && u == -1) break;
+            
+            cnt1 = (u >= 0 ? acc1[u] : 0) - (l >= 1 ? acc1[l-1] : 0);
+            cnt2 = (u >= 0 ? acc2[u] : 0) - (l >= 1 ? acc2[l-1] : 0);
+
+            bw.write(l + " " + u + " " + cnt1 + " " + cnt2);
+            bw.write("\n");
+        }
+
+        bw.flush();
+    }
+
+    public static int readInt() throws IOException {
+        int r = 0, c = System.in.read();
+
+        while (c <= ' ') c = System.in.read();
+        boolean negative = false;
+        if (c == '-') {
+            negative = true;
+            c = System.in.read();
+        }
+        while (c >= '0' && c <= '9') {
+            r *= 10;
+            r += c - '0';
+            c = System.in.read();
+        }
+
+        return negative ? -r : r;
+    }    
+} 
