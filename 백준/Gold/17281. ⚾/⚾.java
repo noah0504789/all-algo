150a07ded7ac491b69f43610a5093bcbd2abc4d5
noah0504789 <58963724+noah0504789@users.noreply.github.com>
2024-11-30 02:34:52 +0900

    [Gold IV] Title: ⚾, Time: 404 ms, Memory: 16540 KB -BaekjoonHub

--- 백준/Gold/17281. ⚾/⚾.java
+++ 백준/Gold/17281. ⚾/⚾.java
@@ -4,11 +4,13 @@ import java.util.*;
 public class Main {
     private static BufferedWriter bw;
 
+    private static final int POINT_BIT = 0b1111000;
     private static int[][] records;
     private static int[] players;
-    private static boolean[] visited, bases;
+//    private static boolean[] visited, bases;
+    private static boolean[] visited;
     private static long max;
-    private static int n, out, record;
+    private static int n, out, record, bases;
 
     public static void main(String... args) throws IOException {
         bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -27,6 +29,8 @@ public class Main {
         visited = new boolean[9];
         visited[3] = true;
 
+//        bases = new boolean[5];
+
         max = 0;
 
         perm(1);
@@ -37,7 +41,7 @@ public class Main {
 
     private static void perm(int player) {
         if (player == 9) {
-            max = Math.max(max, play());
+            max = Math.max(max, play(0, 0, 0));
             return;
         }
 
@@ -51,71 +55,30 @@ public class Main {
         }
     }
 
-    private static long play() {
-        int pIdx = 0, points = 0;
+    private static long play(int inning, int pIdx, long points) {
+        if (inning == n) return points;
 
-        for (int inning = 0; inning < n; inning++) {
-            out = 0;
-            bases = new boolean[5];
+        out = bases = 0;
 
-            while (out < 3) {
-                record = records[inning][players[pIdx]];
+        while (out < 3) {
+            record = records[inning][players[pIdx]];
 
-                if (record == 1) {
-                    for (int k = 3; k >= 1; k--) {
-                        if (!bases[k]) continue;
+            if (record == 0) {
+                out++;
+            } else {
+                bases <<= record;
+                bases |= (1<<(record-1));
 
-                        bases[k] = false;
-                        if (k == 3) {
-                            points++;
-                            continue;
-                        }
-
-                        bases[k+1] = true;
-                    }
-
-                    bases[1] = true;
-                } else if (record == 2) {
-                    for (int k = 3; k >= 1; k--) {
-                        if (!bases[k]) continue;
-
-                        bases[k] = false;
-                        if (k >= 2) {
-                            points++;
-                            continue;
-                        }
-
-                        bases[k+2] = true;
-                    }
-
-                    bases[2] = true;
-                } else if (record == 3) {
-                    for (int k = 3; k >= 1; k--) {
-                        if (!bases[k]) continue;
-
-                        bases[k] = false;
-                        points++;
-                    }
-
-                    bases[3] = true;
-                } else if (record == 4) {
-                    for (int k = 3; k >= 1; k--) {
-                        if (!bases[k]) continue;
-
-                        bases[k] = false;
-                        points++;
-                    }
-
-                    points++;
-                } else if (record == 0) {
-                    out++;
+                if ((bases & POINT_BIT) > 0) {
+                    points += Integer.bitCount((bases & POINT_BIT));
+                    bases &= ~POINT_BIT;
                 }
-
-                pIdx = (pIdx+1) % 9;
             }
+
+            pIdx = (pIdx+1) % 9;
         }
 
-        return points;
+        return play(inning+1, pIdx, points);
     }
 
     public static int readInt() throws IOException {
