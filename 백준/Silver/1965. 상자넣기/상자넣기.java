be7d1bdd18b58c267d5bce085c5eeec5fa702aab
noah0504789 <58963724+noah0504789@users.noreply.github.com>
2024-12-01 14:54:18 +0900

    [Silver II] Title: 상자넣기, Time: 108 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Silver/1965. 상자넣기/상자넣기.java
+++ 백준/Silver/1965. 상자넣기/상자넣기.java
@@ -4,9 +4,9 @@ import java.util.*;
 public class Main {
     private static BufferedWriter bw;
 
-    private static long[] dp;
+    private static List<Integer> list;
+
     private static int[] boxes;
-    private static long max;
     private static int n;
 
     public static void main(String... args) throws IOException {
@@ -16,21 +16,24 @@ public class Main {
         boxes = new int[n];
         for (int i = 0; i < n; i++) boxes[i] = readInt();
 
-        dp = new long[n];
-        
-        max = 0;
-        for (int i = 0; i < n; i++) {
-            dp[i] = 1;
+        list = new ArrayList<>();
 
-            for (int j = 0; j < i; j++) {
-                if (boxes[i] > boxes[j]) dp[i] = Math.max(dp[i], dp[j]+1);
-            }
-            
-            max = Math.max(max, dp[i]);
-        }
-
-        bw.write(max+"");
+        bw.write(lis()+"");
         bw.flush();
+    }
+
+    private static long lis() {
+        list.clear();
+        
+        for (int box : boxes) {
+            int pos = Collections.binarySearch(list, box);
+            
+            if (pos < 0) pos = -(pos+1);
+            if (pos >= list.size()) list.add(box);
+            else list.set(pos, box);
+        }
+        
+        return list.size();
     }
 
     public static int readInt() throws IOException {
