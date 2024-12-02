cb804a384f6fec61c68afdd4c767962a31e18aa1
noah0504789 <58963724+noah0504789@users.noreply.github.com>
2024-12-03 01:59:39 +0900

    [Gold IV] Title: 세 수의 합, Time: 140 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Gold/2295. 세 수의 합/세 수의 합.java
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.java
@@ -2,36 +2,52 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int[] nums = new int[n];
+    private static BufferedWriter bw;
 
-        // 입력받기
-        for (int i = 0; i < n; i++) {
-            nums[i] = Integer.parseInt(br.readLine());
-        }
+    private static int[] nums;
+    private static int n, k, a, b, c;
 
-        // 오름차순 정렬
+    public static void main(String... args) throws IOException {
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        n = readInt();
+
+        nums = new int[n];
+        for (int i = 0; i < n; i++) nums[i] = readInt();
+
         Arrays.sort(nums);
 
-        // 모든 두 수의 합을 저장할 집합
-        Set<Integer> sumSet = new HashSet<>();
-        for (int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) {
-                sumSet.add(nums[i] + nums[j]); // 두 수의 합 계산
-            }
-        }
+        for (int i = n-1; i >= 0; i--) {
+            for (int j = i; j >= 0; j--) {
+                for (int l = j; l >= 0; l--) {
+                    k = nums[i];
+                    a = nums[j];
+                    b = nums[l];
 
-        // 큰 k 탐색
-        for (int i = n - 1; i >= 0; i--) { // c 선택
-            for (int j = i - 1; j >= 0; j--) { // k - c = a + b 조건 확인
-                int diff = nums[i] - nums[j];
-                if (sumSet.contains(diff)) { // k - c가 존재하는 경우
-                    System.out.println(nums[i]); // 가장 큰 k 출력
+                    c = k-a-b;
+
+                    if (c <= 0) break;
+
+                    if (Arrays.binarySearch(nums, c) < 0) continue;
+
+                    bw.write(k+"");
+                    bw.flush();
                     return;
                 }
             }
         }
+    }
+
+    public static int readInt() throws IOException {
+        int r = 0, c = System.in.read();
+
+        while (c <= ' ') c = System.in.read();
+        while (c >= '0' && c <= '9') {
+            r *= 10;
+            r += c - '0';
+            c = System.in.read();
+        }
+
+        return r;
     }
 }
