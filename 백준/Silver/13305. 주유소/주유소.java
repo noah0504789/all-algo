89b9e09aef794439d024f6928ec57b771caaa598
noah0504789 <58963724+noah0504789@users.noreply.github.com>
2024-12-04 14:40:55 +0900

    [Silver III] Title: 주유소, Time: 152 ms, Memory: 16048 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -3,41 +3,49 @@ import java.io.*;
 public class Main {
     private static BufferedWriter bw;
 
-    private static int[] dists, costs;
-    private static int n, min, next, dist;
+    private static long[] dists, costs;
+    private static long total, cost;
+    private static int n;
 
     public static void main(String... args) throws IOException {
         bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         n = readInt();
 
-        dists = new int[n];
-        for (int i = 1; i < n; i++) dists[i] = readInt();
+        dists = new long[n];
+        for (int i = 1; i < n; i++) dists[i] = readLong();
 
-        costs = new int[n+1];
-        for (int i = 1; i <= n; i++) costs[i] = readInt();
+        costs = new long[n+1];
+        for (int i = 1; i <= n; i++) costs[i] = readLong();
 
-        min = 0;
+        cost = costs[1];
+        total = 0;
 
         for (int i = 1; i < n; i++) {
-            dist = dists[i];
-            next = i+1;
+            if (costs[i] < cost) cost = costs[i];
 
-            while (next < n && costs[i] <= costs[next]) dist += dists[next++];
-
-            min += dist * costs[i];
-
-            if (next == n-1) break;
-
-            i = next-1;
+            total += cost * dists[i];
         }
 
-        bw.write(min+"");
+        bw.write(total +"");
         bw.flush();
     }
 
     public static int readInt() throws IOException {
         int r = 0, c = System.in.read();
+
+        while (c <= ' ') c = System.in.read();
+        while (c >= '0' && c <= '9') {
+            r *= 10;
+            r += c - '0';
+            c = System.in.read();
+        }
+
+        return r;
+    }
+
+    public static long readLong() throws IOException {
+        long r = 0, c = System.in.read();
 
         while (c <= ' ') c = System.in.read();
         while (c >= '0' && c <= '9') {
