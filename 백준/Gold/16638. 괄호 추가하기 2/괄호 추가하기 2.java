43b2d6ea39edc7974066b5e26a6752d2e132dafa
noah0504789 <58963724+noah0504789@users.noreply.github.com>
2024-12-07 15:27:31 +0900

    [Gold I] Title: 괄호 추가하기 2, Time: 108 ms, Memory: 14364 KB -BaekjoonHub

--- 백준/Gold/16638. 괄호 추가하기 2/괄호 추가하기 2.java
+++ 백준/Gold/16638. 괄호 추가하기 2/괄호 추가하기 2.java
@@ -2,97 +2,93 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    private static BufferedReader br;
+    private static BufferedWriter bw;
+
     private static List<String> list;
-    static int N, answer = Integer.MIN_VALUE;
-    static String input;
-    static boolean[] bracket;
+    private static String input;
+    private static boolean[] bracket;
+    private static int n, ans;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String... args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        N = Integer.parseInt(br.readLine());
-        bracket = new boolean[N];
+        n = Integer.parseInt(br.readLine());
         input = br.readLine();
+
+        bracket = new boolean[n];
         list = new ArrayList<>();
 
-        if (input.length() == 1)
-            answer = Character.getNumericValue(input.charAt(0));
-        else
-            dfs(0);
+        ans = Integer.MIN_VALUE;
 
-        bw.write(answer + "");
+        if (n == 1) ans = Character.getNumericValue(input.charAt(0));
+        else dfs(0);
+
+        bw.write(ans+"");
         bw.flush();
-        bw.close();
-        br.close();
     }
 
-    static void dfs(int depth) {
-        if (depth == N+1) {
+    private static void dfs(int depth) {
+        if (depth == n+1) {
             express();
-            answer = Math.max(answer, cal2());
+            ans = Math.max(ans, cal2());
             return;
         }
 
-        if (depth == 0) {
+        if (depth == 0 || (depth > 0 && !bracket[depth-2] && depth < n-1)) {
             bracket[depth] = true;
-            dfs(depth + 2);
-            bracket[depth] = false;
-        } else if (!bracket[depth - 2] && depth < N - 1) {
-            bracket[depth] = true;
-            dfs(depth + 2);
+            dfs(depth+2);
             bracket[depth] = false;
         }
 
-        dfs(depth + 2);
+        dfs(depth+2);
     }
 
-    static void express() {
+    private static void express() {
         list.clear();
+
         int idx = 0;
-        while (idx < N) {
+        while (idx < n) {
             char val = input.charAt(idx);
 
             if (val == '+' || val == '-') {
-                list.add(val + "");
+                list.add(val+"");
             } else if (val == '*') {
-                int cur = Integer.parseInt(list.get(list.size() - 1)), next;
+                int cur = Integer.parseInt(list.get(list.size()-1)), next;
 
-                if (bracket[idx + 1]) {
-                    next = cal(input.charAt(idx + 1) - '0', input.charAt(idx + 3) - '0', input.charAt(idx + 2));
-                    idx += 3;
+                if (bracket[idx+1]) {
+                    next = cal1(input.charAt(idx+1) - '0', input.charAt(idx+3) - '0', input.charAt(idx+2));
+                    idx+=3;
                 } else {
-                    next = input.charAt(idx + 1) - '0';
+                    next = input.charAt(idx+1) - '0';
                     idx++;
                 }
 
-                list.set(list.size() - 1, cal(cur, next, val) + "");
+                list.set(list.size()-1, cal1(cur, next, val)+"");
             } else if (bracket[idx]) {
-                list.add(cal(val - '0', input.charAt(idx + 2) - '0', input.charAt(idx + 1)) + "");
-                idx += 2;
+                list.add(cal1(val - '0', input.charAt(idx+2) - '0', input.charAt(idx+1))+"");
+                idx+=2;
             } else {
-                list.add(val + "");
+                list.add(val+"");
             }
 
             idx++;
         }
     }
 
-    static int cal2() {
-        int rst = Integer.parseInt(list.get(0));
+    private static int cal1(int op1, int op2, char op) {
+        if (op == '+') return op1+op2;
+        if (op == '-') return op1-op2;
 
-        for (int i = 1; i < list.size(); i += 2)
-            rst = cal(rst, Integer.parseInt(list.get(i + 1)), list.get(i).charAt(0));
-
-        return rst;
+        return op1*op2;
     }
 
-    static int cal(int n1, int n2, char operation) {
-        if (operation == '+')
-            return n1 + n2;
-        else if (operation == '-')
-            return n1 - n2;
-        else
-            return n1 * n2;
+    private static int cal2() {
+        int rst = Integer.parseInt(list.get(0));
+
+        for (int i = 1; i < list.size(); i+=2) rst = cal1(rst, Integer.parseInt(list.get(i+1)), list.get(i).charAt(0));
+
+        return rst;
     }
 }
