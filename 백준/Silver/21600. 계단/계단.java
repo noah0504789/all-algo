333d3a1f2c662e9295f1e0170ed2ce4bf6ef3eb3
noah0504789 <58963724+noah0504789@users.noreply.github.com>
2025-10-11 21:45:26 +0900

    [Silver I] Title: 계단, Time: 128 ms, Memory: 14332 KB, Score: 150 point -BaekjoonHub

--- 백준/Silver/21600. 계단/계단.java
+++ 백준/Silver/21600. 계단/계단.java
@@ -3,30 +3,20 @@ import java.util.*;
 
 public class Main {
     
-    private static int n, k, l, r, h;
-    private static int[] arr;
+    private static int n, ans, cur;
+    private static boolean flag;
     
     public static void main(String... args) throws IOException {
         n = readInt();
-        arr = new int[n];
-        for (int i = 0; i < n; i++) arr[i] = readInt();
         
-        k = h = 1;
-        l = r = 0;
+        ans = cur = 0;
         
-        while (r+1 < n) {
-            if (arr[r+1] < h+1) {
-                l++;
-                r = l;
-                h = 1;
-            } else if (arr[r+1] >= h+1) {
-                r++;
-                h++;
-                k = Math.max(k, h);
-            }
+        for (int i = 0; i < n; i++) {
+            cur = Math.min(cur+1, readInt());
+            ans = Math.max(ans, cur);
         }
 
-        System.out.print(k);
+        System.out.print(ans);
     }
 
     public static int readInt() throws IOException {
