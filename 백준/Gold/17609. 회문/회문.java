cfde0fa3cabeb1fa78fe1478da030e7e57599a2b
noah0504789 <58963724+noah0504789@users.noreply.github.com>
2025-10-11 22:21:41 +0900

    [Gold V] Title: 회문, Time: 292 ms, Memory: 28912 KB -BaekjoonHub

---
+++ 백준/Gold/17609. 회문/회문.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    
+    private static StringBuilder sb;
+    private static BufferedReader br;
+    private static final int P = 0, S = 1, N = 2;
+    private static int tc;
+    
+    public static void main(String... args) throws IOException {
+        sb = new StringBuilder();
+        
+        br = new BufferedReader(new InputStreamReader(System.in));
+        
+        tc = Integer.parseInt(br.readLine());
+        
+        for (int i = 0; i < tc; i++) {
+            sb.append(solution(br.readLine())).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+    
+    private static int solution(String s) {
+        int l = 0, r = s.length()-1;
+        while (l <= r) {
+            if (s.charAt(l) == s.charAt(r)) {
+                l++;
+                r--;
+            } 
+            else if (isPal(s, l+1, r) || isPal(s, l, r-1)) return S; 
+            else return N;                
+        }
+        
+        return P;
+    }
+    
+    private static boolean isPal(String s, int l, int r) {
+        while (l <= r) {
+            if (s.charAt(l) != s.charAt(r)) return false;
+            l++;
+            r--;
+        }
+        
+        return true;
+    }
+}
