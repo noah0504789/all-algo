15d64c90a672134ad2c2a5548680201603e110c5
noah0504789 <58963724+noah0504789@users.noreply.github.com>
2025-10-13 02:18:54 +0900

    [Gold III] Title: 같이 눈사람 만들래?, Time: 148 ms, Memory: 14332 KB -BaekjoonHub

--- 백준/Gold/20366. 같이 눈사람 만들래？/같이 눈사람 만들래？.java
+++ 백준/Gold/20366. 같이 눈사람 만들래？/같이 눈사람 만들래？.java
@@ -5,7 +5,6 @@ public class Main {
     
     private static int n, ans;
     private static int[] arr;
-    private static ArrayList<Pair> pairs;
     
     public static void main(String... args) throws IOException {
         n = readInt();
@@ -14,43 +13,27 @@ public class Main {
         
         Arrays.sort(arr);
                         
-        pairs = new ArrayList<>();
+        ans = Integer.MAX_VALUE;
         
-        for (int l = 0; l < n-1; l++) {
-            int r = l+1;
-            while (r < n) {
-                int sum = arr[l] + arr[r];
-                pairs.add(new Pair(sum, l, r));      
-                r++;
+        for (int l = 0; l < n-3; l++) {
+            for (int r = l+3; r < n; r++) {
+                snow(l, r);
+                if (ans == 0) break;
             }
         }
         
-        pairs.sort(Comparator.comparingInt(p -> p.sum));
-        
-        ans = Integer.MAX_VALUE;
-        for (int i = 0; i < pairs.size(); i++) {
-            Pair p = pairs.get(i);
-            if (ans == 0) break;
-            for (int j = i+1; j < pairs.size(); j++) {
-                Pair q = pairs.get(j);
-                int diff = q.sum - p.sum;
-                if (diff >= ans) break;
-                if (q.i == p.i || q.i == p.j || q.j == p.i || q.j == p.j) continue;
-                ans = diff;
-                if (ans == 0) break;
-            }
-        }        
-
         System.out.print(ans);
     }
     
-    static class Pair {
-        int sum, i, j;
-        
-        public Pair(int sum, int i, int j) {
-            this.sum = sum;
-            this.i = i;
-            this.j = j;
+    private static void snow(int l, int r) {
+        int endsSum = arr[l] + arr[r];
+        int lr = l+1, rl = r-1;
+        while (lr < rl) {
+            int sum = arr[lr] + arr[rl];
+            ans = Math.min(ans, Math.abs(endsSum-sum));
+            if (sum == endsSum) break;
+            else if (sum > endsSum) rl--;
+            else lr++;
         }
     }
 
