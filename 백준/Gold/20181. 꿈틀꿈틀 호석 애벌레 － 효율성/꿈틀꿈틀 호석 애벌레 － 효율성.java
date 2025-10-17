494a8006d0ffa9375152a4de082897da0b980abf
noah0504789 <58963724+noah0504789@users.noreply.github.com>
2025-10-17 20:23:47 +0900

    [Gold II] Title: 꿈틀꿈틀 호석 애벌레 - 효율성, Time: 108 ms, Memory: 15108 KB, Score: 5 point -BaekjoonHub

---
+++ 백준/Gold/20181. 꿈틀꿈틀 호석 애벌레 － 효율성/꿈틀꿈틀 호석 애벌레 － 효율성.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    
+    private static int n, k, l, sum;
+    private static int[] arr, dp; 
+    
+    public static void main(String... args) throws IOException {
+        n = readInt();
+        k = readInt();
+        arr = new int[n+1];
+        dp = new int[n+1];
+        
+        for (int i = 1; i <= n; i++) arr[i] = readInt();
+        
+        l = 0;
+        sum = 0;
+        for (int r = 1, l = 0; r < n; r++) {
+            sum += arr[r];
+            dp[r] = dp[r-1];
+            while (sum >= k) {
+                dp[r] = Math.max(dp[r], dp[l] + sum-k);
+                sum -= arr[++l];
+            }
+        }
+
+        System.out.print(dp[n]);
+    }
+
+    public static int readInt() throws IOException {
+        int r = 0, c = System.in.read();
+        boolean negative = false;       
+
+        while (c <= ' ') c = System.in.read();
+        if (c == '-') {
+            negative = true;
+            c = System.in.read();
+        }
+        while (c >= '0' && c <= '9') {
+            r *= 10;
+            r += c - '0';
+            c = System.in.read();
+        }
+
+        return negative ? -r : r;
+    }
+}
