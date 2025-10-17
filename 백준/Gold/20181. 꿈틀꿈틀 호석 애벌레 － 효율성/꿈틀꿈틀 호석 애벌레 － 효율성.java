683c1c3e43561e69f9327434244d297b91877c1a
noah0504789 <58963724+noah0504789@users.noreply.github.com>
2025-10-17 20:30:07 +0900

    [Gold II] Title: 꿈틀꿈틀 호석 애벌레 - 효율성, Time: 152 ms, Memory: 15468 KB, Score: 66 point -BaekjoonHub

--- 백준/Gold/20181. 꿈틀꿈틀 호석 애벌레 － 효율성/꿈틀꿈틀 호석 애벌레 － 효율성.java
+++ 백준/Gold/20181. 꿈틀꿈틀 호석 애벌레 － 효율성/꿈틀꿈틀 호석 애벌레 － 효율성.java
@@ -3,24 +3,26 @@ import java.util.*;
 
 public class Main {
     
-    private static int n, k, l, sum;
-    private static int[] arr, dp; 
+    private static int n, l;
+    private static long sum, k;
+    private static int[] arr;
+    private static long[] dp;
     
     public static void main(String... args) throws IOException {
         n = readInt();
         k = readInt();
         arr = new int[n+1];
-        dp = new int[n+1];
+        dp = new long[n+1];
         
         for (int i = 1; i <= n; i++) arr[i] = readInt();
         
         l = 0;
-        sum = 0;
-        for (int r = 1, l = 0; r < n; r++) {
+        sum = 0L;
+        for (int r = 1; r <= n; r++) {
             sum += arr[r];
             dp[r] = dp[r-1];
             while (sum >= k) {
-                dp[r] = Math.max(dp[r], dp[l] + sum-k);
+                dp[r] = Math.max(dp[r], dp[l] + (sum-k));
                 sum -= arr[++l];
             }
         }
