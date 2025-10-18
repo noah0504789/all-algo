21dfbe015b6b09e638426502e6166446228fd05e
noah0504789 <58963724+noah0504789@users.noreply.github.com>
2025-10-18 14:08:07 +0900

    [Silver IV] Title: Sort 마스터 배지훈의 후계자, Time: 668 ms, Memory: 33232 KB -BaekjoonHub

---
+++ 백준/Silver/20551. Sort 마스터 배지훈의 후계자/Sort 마스터 배지훈의 후계자.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    
+    private static StringBuilder sb;
+    private static int n, m;
+    private static int[] arr;
+    
+    public static void main(String... args) throws IOException {
+        sb = new StringBuilder();
+        
+        n = readInt();
+        m = readInt();
+        arr = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = readInt();
+        
+        Arrays.sort(arr);
+        
+        while (m-- > 0) sb.append(lowerBound(readInt())).append("\n");
+
+        System.out.print(sb);
+    }
+    
+    private static int lowerBound(int target) {
+        int l = 0, r = n;
+        boolean flag = false;
+        while (l < r) {
+            int m = (l+r)>>>1;
+            if (arr[m] >= target) {
+                r = m; 
+                if (arr[m] == target) flag = true;
+            }
+            else l = m+1;
+        }
+        return flag ? l : -1;
+    }
+
+    public static int readInt() throws IOException {
+        int r = 0, c = System.in.read();
+        boolean negative = false;       
+
+        while (c <= ' ') c = System.in.read();
+        if (c == '-') {
+            negative = true;
+            c = System.in.read();
+        }
+        while (c >= '0' && c <= '9') {
+            r *= 10;
+            r += c - '0';
+            c = System.in.read();
+        }
+
+        return negative ? -r : r;
+    }
+}
