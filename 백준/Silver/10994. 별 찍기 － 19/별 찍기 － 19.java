739aef1bb158446821672e8db3e465e139c7ba3b
noah0504789 <58963724+noah0504789@users.noreply.github.com>
2025-10-26 01:48:24 +0900

    [Silver IV] Title: 별 찍기 - 19, Time: 140 ms, Memory: 16188 KB -BaekjoonHub

---
+++ 백준/Silver/10994. 별 찍기 － 19/별 찍기 － 19.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    
+    private static StringBuilder sb;
+    private static int n, r, c, width;
+    private static char[][] arr;
+    
+    public static void main(String... args) throws IOException {
+        sb = new StringBuilder();
+        
+        n = readInt();
+        r = c = (n-1)*2;
+        width = 2*r+1;
+        arr = new char[width][width];        
+        arr[r][c] = '*';
+        
+        draw(0, width-1);
+        
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < width; j++) {
+                char val = arr[i][j] == '\u0000' ? ' ' : arr[i][j];
+                sb.append(val);
+            }
+            sb.append("\n");
+        }
+
+        System.out.print(sb);
+    }
+    
+    static void draw(int s, int e) {
+        if (s > e) return;
+        
+        for (int j = s; j <= e; j++) {arr[s][j] = '*'; arr[e][j] = '*';}
+        for (int j = s; j <= e; j++) {arr[j][s] = '*'; arr[j][e] = '*';}
+        
+        draw(s+2, e-2);
+    }
+
+    public static int readInt() throws IOException {
+        int r = 0, c = System.in.read();
+        boolean negative = false;       
+
+        while (c <= ' ') c = System.in.read();
+        if (c == '-') {
+            negative = true;
+            c = System.in.read();
+        }
+        while (c >= '0' && c <= '9') {
+            r *= 10;
+            r += c - '0';
+            c = System.in.read();
+        }
+
+        return negative ? -r : r;
+    }
+}
