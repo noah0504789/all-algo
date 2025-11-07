86829eb03dd6fd9c475484222dcbbfd73f5139c6
noah0504789 <58963724+noah0504789@users.noreply.github.com>
2025-11-07 09:02:26 +0900

    [Silver V] Title: 행렬 곱셈, Time: 152 ms, Memory: 16476 KB -BaekjoonHub

--- 백준/Silver/2740. 행렬 곱셈/행렬 곱셈.java
+++ 백준/Silver/2740. 행렬 곱셈/행렬 곱셈.java
@@ -4,8 +4,8 @@ import java.util.*;
 public class Main {
     
     private static StringBuilder sb;
-    private static int n, m;
-    private static int[][] a, b;
+    private static int n, m, k;
+    private static int[][] a, b, c;
     
     public static void main(String... args) throws IOException {
         sb = new StringBuilder();
@@ -17,25 +17,37 @@ public class Main {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) a[i][j] = readInt();
         }
-        
-        n = readInt();
+                
         m = readInt();
+        k = readInt();
         
-        b = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) b[i][j] = readInt();
+        b = new int[m][k];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < k; j++) b[i][j] = readInt();
         }
         
-        for (int i = 0; i < a.length; i++) {
-            for (int j = 0; j < b[0].length; j++) {
-                int val = 0;
-                for (int k = 0; k < a[0].length; k++) val += (a[i][k] * b[k][j]);
-                sb.append(val+ " ");
-            }
+        c = new int[n][k];
+        
+        multiply(a, b);
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < k; j++) {
+                sb.append(c[i][j]+" ");
+            }    
             sb.append("\n");
         }
 
         System.out.print(sb);
+    }
+    
+    private static void multiply(int[][] a, int[][] b) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < k; j++) {
+                for (int v = 0; v < m; v++) {
+                    c[i][j] += a[i][v] * b[v][j];
+                }    
+            }    
+        }
     }
 
     public static int readInt() throws IOException {
