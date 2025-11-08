00d5ea40157a84116b66bfcff19fa4b9f917c333
noah0504789 <58963724+noah0504789@users.noreply.github.com>
2025-11-09 01:14:55 +0900

    [Gold IV] Title: MVP 다이아몬드 (Normal), Time: 164 ms, Memory: 16036 KB -BaekjoonHub

---
+++ 백준/Gold/20414. MVP 다이아몬드 （Normal）/MVP 다이아몬드 （Normal）.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    
+    private static BufferedReader br;
+    private static int n, s, g, p, d;
+    private static long ans;
+    private static char[] arr;
+    private static String[] input;
+    private static int[] l, u;
+    private static int[][] dp;     
+    
+    public static void main(String... args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        
+        n = Integer.parseInt(br.readLine());
+
+        input = br.readLine().split(" ");     
+        s = Integer.parseInt(input[0]);
+        g = Integer.parseInt(input[1]);
+        p = Integer.parseInt(input[2]);
+        d = Integer.parseInt(input[3]);
+        
+        l = new int[n+1];
+        u = new int[n+1];
+        
+        arr = br.readLine().toCharArray();
+        for (int i = 1; i <= n; i++) {
+            switch (arr[i-1]) {
+                case 'B': l[i] = 0; u[i] = s-1; break;
+                case 'S': l[i] = s; u[i] = g-1; break;
+                case 'G': l[i] = g; u[i] = p-1; break;
+                case 'P': l[i] = p; u[i] = d-1; break;
+                case 'D': l[i] = d; u[i] = 2*d; break;
+            }
+        }
+        
+        dp = new int[n+1][d+1];
+        for (int i = 0; i <= n; i++) Arrays.fill(dp[i], -1_000_000_000);
+        
+        for (int x1 = 0; x1<=d; x1++) {
+            int sum = x1;
+            if (l[1] <= sum && sum <= u[1]) dp[1][x1] = x1;
+        }
+        
+        for (int i = 2; i <= n; i++) {
+            for (int prev = 0; prev <= d; prev++) {
+                if (dp[i-1][prev] == -1_000_000_000) continue;
+                
+                int lo = Math.max(0, l[i]-prev);
+                int hi = Math.min(d, u[i]-prev);
+                if (lo > hi) continue;
+                for (int cur = lo; cur <= hi; cur++) dp[i][cur] = Math.max(dp[i][cur], dp[i-1][prev] + cur);
+            }
+        }
+                
+        int ans = 0;
+        for (int last = 0; last <= d; last++) ans = Math.max(ans, dp[n][last]);
+        
+        System.out.print(ans);
+    }    
+}
