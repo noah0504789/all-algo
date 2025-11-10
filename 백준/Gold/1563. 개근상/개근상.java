ddad74cc109669a17413dcd164e66eb274051f44
noah0504789 <58963724+noah0504789@users.noreply.github.com>
2025-11-10 18:25:14 +0900

    [Gold IV] Title: 개근상, Time: 104 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Gold/1563. 개근상/개근상.java
+++ 백준/Gold/1563. 개근상/개근상.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class Main {
     
-    private static int n, ans, p = 1_000_000;
+    private static int n, p = 1_000_000, ans;
     private static int[][] prev, next;
     
     public static void main(String... args) throws IOException {
@@ -14,27 +14,22 @@ public class Main {
         
         for (int day = 0; day < n; day++) {
             next = new int[2][3];
-            
             for (int i = 0; i < 2; i++) {
                 for (int j = 0; j < 3; j++) {
                     int cur = prev[i][j];
                     if (cur == 0) continue;
-                    
+                
                     next[i][0] = (next[i][0]+cur)%p;
                     
                     if (j < 2) next[i][j+1] = (next[i][j+1]+cur)%p;
-                    if (i < 1) next[i+1][0] = (next[i+1][0]+cur)%p;
-                }
+                    if (i == 0) next[i+1][0] = (next[i+1][0]+cur)%p;
+                }    
             }
             
             prev = next;
         }
         
-        for (int[] v : prev) {
-            ans += v[0]%p;
-            ans += v[1]%p;
-            ans += v[2]%p;
-        }
+        for(int[] v : prev) ans += (v[0]+v[1]+v[2]) % p;
 
         System.out.print(ans % p);
     }
