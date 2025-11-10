add1899cb215e69a975b334dac01fc0a6d797ac4
noah0504789 <58963724+noah0504789@users.noreply.github.com>
2025-11-11 03:41:13 +0900

    [Silver II] Title: 제곱수의 합, Time: 140 ms, Memory: 14504 KB -BaekjoonHub

---
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    
+    private static int n, ans, INF = Integer.MAX_VALUE/2;
+    private static int[] dp;
+    
+    public static void main(String... args) throws IOException {
+        n = readInt();
+               
+        dp = new int[n+1];
+        Arrays.fill(dp, INF);
+        dp[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j*j<=i; j++) dp[i] = Math.min(dp[i], dp[i-j*j]+1);
+        }
+        
+        System.out.print(dp[n]);
+    }
+
+    public static int readInt() throws IOException {
+        int r = 0, c = System.in.read();
+        boolean negative = false;       
+
+        while (c <= ' ') c = System.in.read();
+        if (c == '-') {
+            negative = true;
+            c = System.in.read();
+        }
+        while (c >= '0' && c <= '9') {
+            r *= 10;
+            r += c - '0';
+            c = System.in.read();
+        }
+
+        return negative ? -r : r;
+    }
+}
