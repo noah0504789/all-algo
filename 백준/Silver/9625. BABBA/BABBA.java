dcec883e2b1462c64e611eae1fd43510f69afeec
noah0504789 <58963724+noah0504789@users.noreply.github.com>
2025-11-11 09:23:28 +0900

    [Silver V] Title: BABBA, Time: 124 ms, Memory: 15848 KB -BaekjoonHub

---
+++ 백준/Silver/9625. BABBA/BABBA.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    
+    private static int k, a, b, na, nb;
+    
+    public static void main(String... args) throws IOException {
+        k = readInt();
+        
+        a = 1;
+        b = 0;
+        for (int i = 0; i < k; i++) {
+            na = b;
+            nb = a+b;
+            a = na;
+            b = nb;
+        }
+
+        System.out.print(a + " " + b);
+    }   
+
+    public static int readInt() throws IOException {
+        int r = 0, c = System.in.read();
+        boolean negative = false;       
+
+        while (c <= ' ') c = System.in.read();
+        if (c == '-') {
+            negative = true;
+            c = System.in.read();
+        }
+        while (c >= '0' && c <= '9') {
+            r *= 10;
+            r += c - '0';
+            c = System.in.read();
+        }
+
+        return negative ? -r : r;
+    }
+}
