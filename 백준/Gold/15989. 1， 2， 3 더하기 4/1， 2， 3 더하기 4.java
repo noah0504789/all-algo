a81593a24e03d8b3df006e49d65960fe2a5549ca
noah0504789 <58963724+noah0504789@users.noreply.github.com>
2025-11-12 02:04:11 +0900

    [Gold V] Title: 1, 2, 3 더하기 4, Time: 120 ms, Memory: 16256 KB -BaekjoonHub

--- 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.java
+++ 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.java
@@ -6,35 +6,31 @@ public class Main {
     private static StringBuilder sb;
     private static int tc, maxN;
     private static int[] arr;
-    private static int[][] dp;
+    private static long[][] dp;
     
     public static void main(String... args) throws IOException {
         sb = new StringBuilder();
-        
         tc = readInt();
         arr = new int[tc];
-        for (int i = 0; i < tc; i++) {
-            arr[i] = readInt();
-            maxN = Math.max(maxN, arr[i]);
-        }
+        for (int i = 0; i < tc; i++) {arr[i] = readInt(); maxN = Math.max(maxN, arr[i]);}
         
-        dp = new int[Math.max(3, maxN)+1][4];
+        dp = new long[Math.max(3, maxN)+1][4];
         dp[1][1] = 1;
         dp[2][1] = dp[2][2] = 1;
         dp[3][1] = dp[3][2] = dp[3][3] = 1;
-        
         for (int i = 4; i <= maxN; i++) {
             dp[i][1] = dp[i-1][1];
-            dp[i][2] = dp[i-2][1] + dp[i-2][2];
-            dp[i][3] = dp[i-3][1] + dp[i-3][2] + dp[i-3][3];
+            dp[i][2] = (dp[i-2][1] + dp[i-2][2]);
+            dp[i][3] = (dp[i-3][1] + dp[i-3][2] + dp[i-3][3]);
         }
         
-        for (int n : arr) {
-            int ans = dp[n][1] + dp[n][2] + dp[n][3];
-            sb.append(ans+"\n");
-        }
+        for (int i = 0; i < tc; i++) sb.append(sum(arr[i])+"\n");
 
         System.out.print(sb);
+    }
+    
+    private static long sum(int i) {
+        return dp[i][1] + dp[i][2] + dp[i][3];
     }
 
     public static int readInt() throws IOException {
