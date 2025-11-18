9eac3413b3139c46959d388a451d094d66c93ee2
noah0504789 <58963724+noah0504789@users.noreply.github.com>
2025-11-18 19:01:00 +0900

    [Gold V] Title: 연속합 2, Time: 136 ms, Memory: 15600 KB -BaekjoonHub

--- 백준/Gold/13398. 연속합 2/연속합 2.java
+++ 백준/Gold/13398. 연속합 2/연속합 2.java
@@ -1,37 +1,29 @@
- import java.io.*;
+import java.io.*;
 import java.util.*;
 
 public class Main {
     
     private static int n, ans;
-    private static int[] arr;
-    private static int[] left, right;
+    private static int[] arr, left, right;
     
     public static void main(String... args) throws IOException {
         n = readInt();
         
-        arr = new int[n];
+        arr = new int[n];        
         for (int i = 0; i < n; i++) arr[i] = readInt();
         
-        if (n == 1) {
-            System.out.print(arr[0]);    
-            return;
-        }
-
         left = new int[n];
-        right = new int[n];
-        
         left[0] = arr[0];
-        for (int i = 1; i < n; i++) left[i] = Math.max(arr[i], left[i-1]+arr[i]);
+        for (int i = 1; i < n; i++) left[i] = Math.max(arr[i], left[i-1] + arr[i]);
         
+        right = new int[n];
         right[n-1] = arr[n-1];
-        for (int i = n-2; i >= 0; i--) right[i] = Math.max(arr[i], right[i+1]+arr[i]);
+        for (int i = n-2; i >= 0; i--) right[i] = Math.max(arr[i], right[i+1] + arr[i]);
         
         ans = Integer.MIN_VALUE;
         for (int i = 0; i < n; i++) ans = Math.max(ans, left[i]);
-        
         for (int i = 1; i < n-1; i++) ans = Math.max(ans, left[i-1]+right[i+1]);
-        
+
         System.out.print(ans);
     }
 
