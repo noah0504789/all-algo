2a26d7ae5d53e0ab82821ec7cbf66e62c62a31ff
noah0504789 <58963724+noah0504789@users.noreply.github.com>
2025-11-19 07:42:31 +0900

    [Gold II] Title: 로봇 조종하기, Time: 268 ms, Memory: 37100 KB -BaekjoonHub

--- 백준/Gold/2169. 로봇 조종하기/로봇 조종하기.java
+++ 백준/Gold/2169. 로봇 조종하기/로봇 조종하기.java
@@ -3,35 +3,35 @@ import java.util.*;
 
 public class Main {
     
-    private static int n, m;
-    private static int[][] arr;
+    private static int n, m, max, INF = Integer.MAX_VALUE;
     private static long[] dp, left, right;
+    private static int[][] arr;
     
     public static void main(String... args) throws IOException {
         n = readInt();
         m = readInt();
-        
-        arr = new int[n][m];        
+        arr = new int[n][m];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) arr[i][j] = readInt();
         }
         
         dp = new long[m];
         dp[0] = arr[0][0];
-        for (int j = 1; j < m; j++) dp[j] = dp[j-1]+arr[0][j];
-
-        left = new long[m];
-        right = new long[m];
+        for (int i = 1; i < m; i++) dp[i] = dp[i-1]+arr[0][i];
+        
         for (int i = 1; i < n; i++) {
-            left[0] = dp[0] + arr[i][0];
+            left = new long[m];
+            right = new long[m];
+            
+            left[0] = arr[i][0] + dp[0];
             for (int j = 1; j < m; j++) left[j] = Math.max(left[j-1], dp[j]) + arr[i][j];
             
-            right[m-1] = dp[m-1] + arr[i][m-1];
+            right[m-1] = arr[i][m-1] + dp[m-1];
             for (int j = m-2; j >= 0; j--) right[j] = Math.max(right[j+1], dp[j]) + arr[i][j];
             
             for (int j = 0; j < m; j++) dp[j] = Math.max(left[j], right[j]);
         }
-        
+
         System.out.print(dp[m-1]);
     }
 
