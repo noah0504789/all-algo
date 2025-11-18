f5d086b279031ba3f94e1f7e09f473d5a0f76379
noah0504789 <58963724+noah0504789@users.noreply.github.com>
2025-11-18 18:30:55 +0900

    [Silver IV] Title: 연속부분최대곱, Time: 160 ms, Memory: 19496 KB -BaekjoonHub

--- 백준/Silver/2670. 연속부분최대곱/연속부분최대곱.java
+++ 백준/Silver/2670. 연속부분최대곱/연속부분최대곱.java
@@ -3,30 +3,42 @@ import java.util.*;
 
 public class Main {
     
-    private static BufferedReader br;
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     private static int n;
+    private static double ans;
     private static double[] arr, dp;
     
     public static void main(String... args) throws IOException {
-        br = new BufferedReader(new InputStreamReader(System.in));
-        
         n = Integer.parseInt(br.readLine());
         
         arr = new double[n];
-        for (int i = 0; i < n ; i++) arr[i] = Double.parseDouble(br.readLine());
+        for (int i = 0; i < n; i++) arr[i] = Double.parseDouble(br.readLine());
         
         dp = new double[n];
-        int l = 0;
-        double mul = arr[l], ans = 0.0d;
-        for (int r = 1; r < n; r++) {
-            double cur = arr[r];
-            mul *= cur;
-            
-            if (mul < cur) mul = cur;
-            
-            ans = Math.max(ans, mul);
+        dp[0] = arr[0];
+        for (int i = 1; i < n; i++) {
+            dp[i] = Math.max(dp[i-1]*arr[i], arr[i]);
+            ans = Math.max(ans, dp[i]);
         }
 
-        System.out.print(String.format("%.3f", ans));
+        System.out.printf("%.3f", ans);
+    }
+
+    public static int readInt() throws IOException {
+        int r = 0, c = System.in.read();
+        boolean negative = false;       
+
+        while (c <= ' ') c = System.in.read();
+        if (c == '-') {
+            negative = true;
+            c = System.in.read();
+        }
+        while (c >= '0' && c <= '9') {
+            r *= 10;
+            r += c - '0';
+            c = System.in.read();
+        }
+
+        return negative ? -r : r;
     }
 }
