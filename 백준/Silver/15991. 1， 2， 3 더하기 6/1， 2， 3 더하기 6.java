13862efd3cf2badbaf9778d5133613efd46414ba
noah0504789 <58963724+noah0504789@users.noreply.github.com>
2025-11-20 22:22:48 +0900

    [Silver I] Title: 1, 2, 3 더하기 6, Time: 140 ms, Memory: 16884 KB -BaekjoonHub

--- 백준/Silver/15991. 1， 2， 3 더하기 6/1， 2， 3 더하기 6.java
+++ 백준/Silver/15991. 1， 2， 3 더하기 6/1， 2， 3 더하기 6.java
@@ -3,27 +3,33 @@ import java.util.*;
 
 public class Main {
     
-    private static StringBuilder sb = new StringBuilder();
-    private static int tc, maxN, p = 1_000_000_009;
-    private static int[] arr;
+    private static StringBuilder sb;
+    private static int tc, n, maxN, p = 1_000_000_009;
+    private static int[] tArr;
     private static long[] dp;
     
     public static void main(String... args) throws IOException {
+        sb = new StringBuilder();
+        
         tc = readInt();
-        arr = new int[tc];
+        tArr = new int[tc];
         for (int i = 0; i < tc; i++) {
-            arr[i] = readInt();
-            maxN = Math.max(maxN, arr[i]);
+            tArr[i] = readInt();
+            maxN = Math.max(maxN, tArr[i]);
         }
         
-        dp = new long[Math.max(3, maxN)+1];
+        dp = new long[Math.max(4, maxN)+1];
         dp[0] = 1;
         dp[1] = 1;
         dp[2] = 2;
         dp[3] = 2;
-        for (int i = 4; i <= maxN; i++) dp[i] = (dp[i-2] + dp[i-4] + (i-6>=0 ? dp[i-6] : 0)) % p;
+        dp[4] = 3;
+        for (int i = 5; i <= maxN; i++) {
+            dp[i] = dp[i-2] + dp[i-4] + (i-6>= 0 ? dp[i-6] : 0);
+            dp[i] %= p;
+        }
         
-        for (int t : arr) sb.append(dp[t]+"\n");
+        for (int t : tArr) sb.append(dp[t]+"\n");
 
         System.out.print(sb);
     }
