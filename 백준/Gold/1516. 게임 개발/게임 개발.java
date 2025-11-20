2626ea5def2dd8783e6aa6a9f6a0a3b463da4a9c
noah0504789 <58963724+noah0504789@users.noreply.github.com>
2025-11-20 22:38:31 +0900

    [Gold III] Title: 게임 개발, Time: 160 ms, Memory: 16968 KB -BaekjoonHub

--- 백준/Gold/1516. 게임 개발/게임 개발.java
+++ 백준/Gold/1516. 게임 개발/게임 개발.java
@@ -5,42 +5,34 @@ public class Main {
     
     private static StringBuilder sb;
     private static int n, p;
-    private static int[] time, dp;
-    private static List<Integer>[] prev;
+    private static int[] time;
+    private static List<Integer>[] pre;
+    private static long[] dp;
     
     public static void main(String... args) throws IOException {
         sb = new StringBuilder();
-        
         n = readInt();
-        prev = new ArrayList[n+1];
-        for (int i = 1; i <= n; i++) prev[i] = new ArrayList<>();
-        
         time = new int[n+1];
-        for (int i = 1; i <= n; i++) {
+        pre = new ArrayList[n+1];        
+        for (int i = 1; i <= n; i++) {            
             time[i] = readInt();
-            while ((p = readInt()) != -1) prev[i].add(p);
+            pre[i] = new ArrayList<>();
+            while ((p=readInt())!= -1) pre[i].add(p);            
         }
         
-        dp = new int[n+1];
-        for (int i = 1; i <= n; i++) {
-            //int max = 0;
-            //for (int p_ : prev[i]) max = Math.max(max, dp[p_]);
-            //dp[i] = max + time[i];            
-            
-            sb.append(dfs(i)).append("\n");
-        }
-        
+        dp = new long[n+1];
+        for (int i = 1; i <= n; i++) sb.append(dfs(i)+"\n");
+
         System.out.print(sb);
     }
     
-    private static int dfs(int x) {
+    private static long dfs(int x) {
         if (dp[x]>0) return dp[x];
-        int max = 0;
-        for (int p_ : prev[x]) {
-            max = Math.max(max, dfs(p_));
-        }
         
-        return dp[x] = max+time[x];
+        long max = 0;
+        for (int p_ : pre[x]) max = Math.max(max, dfs(p_));
+        
+        return dp[x] = max + time[x];
     }
 
     public static int readInt() throws IOException {
