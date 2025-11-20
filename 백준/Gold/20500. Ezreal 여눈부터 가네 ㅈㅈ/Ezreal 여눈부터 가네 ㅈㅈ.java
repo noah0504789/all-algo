d33ec52f585b4612005d5813efed57fa7732f10f
noah0504789 <58963724+noah0504789@users.noreply.github.com>
2025-11-21 07:48:35 +0900

    [Gold V] Title: Ezreal 여눈부터 가네 ㅈㅈ, Time: 100 ms, Memory: 14120 KB -BaekjoonHub

--- 백준/Gold/20500. Ezreal 여눈부터 가네 ㅈㅈ/Ezreal 여눈부터 가네 ㅈㅈ.java
+++ 백준/Gold/20500. Ezreal 여눈부터 가네 ㅈㅈ/Ezreal 여눈부터 가네 ㅈㅈ.java
@@ -4,21 +4,15 @@ import java.util.*;
 public class Main {
     
     private static int n, p = 1_000_000_007;
-    private static long[][] dp; // n, 각 자리수를 3으로 나눈 나머지
+    private static long[][] dp;
     
     public static void main(String... args) throws IOException {
         n = readInt();
-        if (n == 1) {
-            System.out.print(0);
-            return;
-        }
-        
-        dp = new long[n+1][3];        
-        dp[2][0] = dp[2][1] = 1;
-
-        for (int i = 3; i <= n; i++) {
-            dp[i][0] = (dp[i-1][1] + dp[i-1][2]) % p; // 맨 앞자리를 1 or 5로 둘 때 
-            dp[i][1] = (dp[i-1][0] + dp[i-1][2]) % p;
+        dp = new long[n+1][3];
+        dp[1][2] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i][0] = (dp[i-1][1] + dp[i-1][2]) % p;
+            dp[i][1] = (dp[i-1][2] + dp[i-1][0]) % p;
             dp[i][2] = (dp[i-1][0] + dp[i-1][1]) % p;
         }
 
