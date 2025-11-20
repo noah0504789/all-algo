9a0f567292dac50ba4d64b0ce627072a9d90c81d
noah0504789 <58963724+noah0504789@users.noreply.github.com>
2025-11-20 22:14:03 +0900

    [Gold III] Title: 색상환, Time: 120 ms, Memory: 17220 KB -BaekjoonHub

--- 백준/Gold/2482. 색상환/색상환.java
+++ 백준/Gold/2482. 색상환/색상환.java
@@ -3,38 +3,29 @@ import java.util.*;
 
 public class Main {
     
-    private static int n, k, ans, p = 1_000_000_003;
-    private static int[][] dp;
+    private static int n, k, p = 1_000_000_003;
+    private static int[][] dp; // n개, k개, 0-첫번째에 두기/1-두번쨰에 두기
     
     public static void main(String... args) throws IOException {
         n = readInt();
         k = readInt();
         
-        if (k>n/2) {
-            System.out.print(0);
-            return;
-        }
-        
-        if (k==0) {
-            System.out.print(1);
-            return;
-        }
-        
-        dp = new int[n+1][k+1];        
-        for (int i = 0; i <= n; i++) dp[i][0] = 1;
-        if (n>=1 && k>=1) dp[1][1] = 1;
+        dp = new int[n+1][k+1];
+        for (int i = 1; i <= n; i++) dp[i][0] = 1;
+        dp[1][1] = 1;
         
         for (int i = 2; i <= n; i++) {
-            int upTo = Math.min(k, (i+1)/2);
             dp[i][1] = i;
-            for (int j= 2; j <= upTo; j++) dp[i][j] = (dp[i-1][j] + dp[i-2][j-1]) % p;
+            for (int j = 2; j <= k; j++) {
+                dp[i][j] += dp[i-1][j] + dp[i-2][j-1];
+                dp[i][j] %= p;
+            }
         }
         
-        int part1 = (k-1>=0 && n-3>=0) ? dp[n-3][k-1] : 0;
-        int part2 = dp[n-1][k];
-        int ans = (part1 + part2) % p;
-        
-        System.out.print(ans);
+        int choose_first = n-3>=0 && k-1>= 0 ? dp[n-3][k-1] : 0;
+        int choose_second = n-1>=0? dp[n-1][k] : 0;
+
+        System.out.print((choose_first + choose_second) % p);
     }
 
     public static int readInt() throws IOException {
