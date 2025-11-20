140169251887bf9ff102f2da4f0e36d1ce261328
noah0504789 <58963724+noah0504789@users.noreply.github.com>
2025-11-20 21:45:37 +0900

    [Gold II] Title: 게임, Time: 132 ms, Memory: 16300 KB -BaekjoonHub

--- 백준/Gold/1103. 게임/게임.java
+++ 백준/Gold/1103. 게임/게임.java
@@ -3,20 +3,16 @@ import java.util.*;
 
 public class Main {
     
-    private static BufferedReader br;
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     private static String[] input;
     private static int n, m;
-    private static boolean inf;
-    private static boolean[][] inStack;
     private static char[][] arr;
-    private static int[][] dp; // dest, prev
-    private static int[][] dir = {
-        {0, 1}, {1, 0}, {0, -1}, {-1, 0}
+    private static boolean[][] vis;
+    private static int[][] dp, dir = {
+        {0, 1}, {0, -1}, {1, 0}, {-1, 0}
     };
     
     public static void main(String... args) throws IOException {
-        br = new BufferedReader(new InputStreamReader(System.in));
-        
         input = br.readLine().split(" ");
         n = Integer.parseInt(input[0]);
         m = Integer.parseInt(input[1]);
@@ -24,37 +20,28 @@ public class Main {
         arr = new char[n][m];
         for (int i = 0; i < n; i++) arr[i] = br.readLine().toCharArray();
         
-        dp = new int[n][m];
-        //for (int[] row : dp) Arrays.fill(row, -1);
-        inStack = new boolean[n][m];
+        dp = new int[n][m];        
+        vis = new boolean[n][m];
         
-        int ans = dfs(0, 0);
-        
-        System.out.print(inf ? -1 : ans);
+        System.out.print(dfs(0, 0));
     }
     
     private static int dfs(int r, int c) {
         if (r < 0 || r >= n || c < 0 || c >= m) return 0;
         if (arr[r][c] == 'H') return 0;
-        if (inf) return 0;
         if (dp[r][c] > 0) return dp[r][c];
-        if (inStack[r][c]) {
-            inf = true;
-            return 0;
+        if (vis[r][c]) {
+            System.out.print(-1);
+            System.exit(0);
         }
         
-        inStack[r][c] = true;
-                
-        int w = arr[r][c] - '0';
-        int best = 0;
+        vis[r][c] = true;
+        int max = 0, w = arr[r][c]-'0';
         for (int[] d : dir) {
-            int nr = r + d[0] * w, nc = c + d[1] * w;
-                        
-            best = Math.max(best, 1 + dfs(nr, nc));
-            if (inf) break;            
-        }    
+            int nr = r + d[0]*w, nc = c + d[1]*w;
+            max = Math.max(max, dfs(nr, nc));
+        }
         
-        inStack[r][c] = true;
-        return dp[r][c] = best;
+        return dp[r][c] = max+1;
     }
 }
