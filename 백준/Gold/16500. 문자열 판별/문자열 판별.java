9aff23f78731ceb650e900e3e0aa3a6d7c08b780
noah0504789 <58963724+noah0504789@users.noreply.github.com>
2025-11-21 08:30:55 +0900

    [Gold V] Title: 문자열 판별, Time: 100 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Gold/16500. 문자열 판별/문자열 판별.java
+++ 백준/Gold/16500. 문자열 판별/문자열 판별.java
@@ -4,33 +4,30 @@ import java.util.*;
 public class Main {
     
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    private static int n, m;
-    private static String a;
-    private static String[] s;
+    private static int n;
+    private static String s;
+    private static String[] a;
     private static Boolean[] dp;
     
     public static void main(String... args) throws IOException {
-        a = br.readLine();
-        m = a.length();
+        s = br.readLine();
         n = Integer.parseInt(br.readLine());
-        s = new String[n];
-        for (int i = 0; i < n; i++) s[i] = br.readLine();
-        
-        dp = new Boolean[m+1];
+        a = new String[n];
+        for (int i = 0; i < n; i++) a[i] = br.readLine();
+        dp = new Boolean[s.length()];
 
         System.out.print(dfs(0) ? 1 : 0);
     }
     
     private static boolean dfs(int i) {
-        if (i == m) return true;
+        if (i == s.length()) return true;
         if (dp[i] != null) return dp[i];
-                
-        for (String s_ : s) {
-            if (i+s_.length() > m) continue;
-            if (!a.startsWith(s_, i)) continue;
-
-            int ni = i+s_.length();
-            if (dfs(ni)) return dp[i] = true;                
+        
+        for (String a_ : a) {
+            if (i+a_.length()>s.length()) continue;
+            if (!s.startsWith(a_, i)) continue;
+            
+            if (dfs(i+a_.length())) return dp[i] = true;
         }
         
         return dp[i] = false;
