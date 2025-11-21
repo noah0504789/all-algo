a46a8bb2980bb5d27ff7f267168e077eb299ce22
noah0504789 <58963724+noah0504789@users.noreply.github.com>
2025-11-21 18:29:59 +0900

    [Gold III] Title: 유전자, Time: 180 ms, Memory: 16076 KB -BaekjoonHub

--- 백준/Gold/2306. 유전자/유전자.java
+++ 백준/Gold/2306. 유전자/유전자.java
@@ -6,24 +6,24 @@ public class Main {
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     private static int n;
     private static char[] arr;
-    private static int[][] dp; 
+    private static int[][] dp; // l, r: 구간별 KOI 문자열 최대값
     
     public static void main(String... args) throws IOException {
         arr = br.readLine().toCharArray();
         n = arr.length;
-        
         dp = new int[n][n];
+        
         for (int len = 2; len <= n; len++) {
-            for (int i = 0; i + len-1 < n; i++) {
+            for (int i = 0; i +len -1 < n; i++) {
                 int j = i+len-1;
                 
-                dp[i][j] = dp[i+1][j];
+                dp[i][j] = dp[i+1][j];                
                 for (int k = i; k <= j; k++) {
                     if (!isPair(arr[i], arr[k])) continue;
+                    int inside = k-1>=0 ? dp[i+1][k-1] : 0;
+                    int right = k+1 < n ? dp[k+1][j] : 0;
                     
-                    int inside = (i+1 <= k-1) ? dp[i+1][k-1] : 0;
-                    int right = (k+1<=j) ? dp[k+1][j]: 0;
-                    dp[i][j] = Math.max(dp[i][j], inside+right+2);
+                    dp[i][j] = Math.max(dp[i][j], inside+2+right);
                 }
             }
         }
@@ -32,7 +32,7 @@ public class Main {
     }
     
     private static boolean isPair(char a, char b) {
-        return (a == 'a' && b == 't') || (a=='g'&&b=='c');
+        return (a=='a' && b=='t') || (a=='g'&&b=='c');
     }
 
     public static int readInt() throws IOException {
