0df20671eacd60e835f11b5cd3400b933aaea6a9
noah0504789 <58963724+noah0504789@users.noreply.github.com>
2025-11-21 18:15:50 +0900

    [Gold III] Title: 유전자, Time: 184 ms, Memory: 15520 KB -BaekjoonHub

---
+++ 백준/Gold/2306. 유전자/유전자.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static int n;
+    private static char[] arr;
+    private static int[][] dp; 
+    
+    public static void main(String... args) throws IOException {
+        arr = br.readLine().toCharArray();
+        n = arr.length;
+        
+        dp = new int[n][n];
+        for (int len = 2; len <= n; len++) {
+            for (int i = 0; i + len-1 < n; i++) {
+                int j = i+len-1;
+                
+                dp[i][j] = dp[i+1][j];
+                for (int k = i; k <= j; k++) {
+                    if (!isPair(arr[i], arr[k])) continue;
+                    
+                    int inside = (i+1 <= k-1) ? dp[i+1][k-1] : 0;
+                    int right = (k+1<=j) ? dp[k+1][j]: 0;
+                    dp[i][j] = Math.max(dp[i][j], inside+right+2);
+                }
+            }
+        }
+
+        System.out.print(dp[0][n-1]);
+    }
+    
+    private static boolean isPair(char a, char b) {
+        return (a == 'a' && b == 't') || (a=='g'&&b=='c');
+    }
+
+    public static int readInt() throws IOException {
+        int r = 0, c = System.in.read();
+        boolean negative = false;       
+
+        while (c <= ' ') c = System.in.read();
+        if (c == '-') {
+            negative = true;
+            c = System.in.read();
+        }
+        while (c >= '0' && c <= '9') {
+            r *= 10;
+            r += c - '0';
+            c = System.in.read();
+        }
+
+        return negative ? -r : r;
+    }
+}
