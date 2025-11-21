c58614bdc78f6dac8b819c66e1469963848e7fdc
noah0504789 <58963724+noah0504789@users.noreply.github.com>
2025-11-21 19:02:56 +0900

    [Silver I] Title: 비밀번호, Time: 120 ms, Memory: 14428 KB -BaekjoonHub

--- 백준/Silver/2780. 비밀번호/비밀번호.java
+++ 백준/Silver/2780. 비밀번호/비밀번호.java
@@ -3,27 +3,23 @@ import java.util.*;
 
 public class Main {
     
-    private static StringBuilder sb;
-    private static int tc, n, m, maxN, p = 1_234_567;
+    private static StringBuilder sb = new StringBuilder();
+    private static int tc, n, maxN, p = 1_234_567;
     private static int[] tArr;
-     // i, 좌표
     private static int[][] dp, adj = {
         {7},
         {2, 4},
         {1, 3, 5},
         {2, 6},
         {1, 5, 7},
-        {2, 4, 6, 8},
-        {3, 5, 9},
-        {4, 8, 0},
-        {5, 7, 9},
-        {6,8}
+        {2,4,6,8},
+        {3,5,9},
+        {4,8,0},
+        {5,7,9},
+        {6,8},
     };
-    private static BitSet vis;
     
     public static void main(String... args) throws IOException {
-        sb = new StringBuilder();
-        
         tc = readInt();
         tArr = new int[tc];
         for (int i = 0; i < tc; i++) {
@@ -31,34 +27,28 @@ public class Main {
             maxN = Math.max(maxN, tArr[i]);
         }
         
-        n = 4;
-        m = 3;
         dp = new int[maxN+1][10];
-        vis = new BitSet();
-        for (int i = 0; i < 10; i++) dp[1][i] = 1;
-        
-        for (int len = 1; len < maxN; len++) {
-            for (int cur = 0; cur < 10; cur++) {
-                long val = dp[len][cur];
-                if (val == 0) continue;
-                for (int nxt : adj[cur]) {
-                    dp[len+1][nxt] += val;
-                    dp[len+1][nxt] %= p;
-                }
+        for (int d = 0; d < 10; d++) dp[1][d] = 1;
+        for (int i = 1; i < maxN; i++) {
+            for (int d= 0; d <10; d++) {
+                for (int nxt : adj[d]) {
+                    dp[i+1][nxt] += dp[i][d];
+                    dp[i+1][nxt] %= p;
+                }                
             }
         }
+        
         for (int t : tArr) {
             int sum = 0;
-            for (int i = 0; i < 10; i++) {
-                sum += dp[t][i];
+            for (int d = 0; d < 10; d++) {
+                sum += dp[t][d];
                 sum %= p;
             }
-            
             sb.append(sum+"\n");
         }
 
         System.out.print(sb);
-    }    
+    }
 
     public static int readInt() throws IOException {
         int r = 0, c = System.in.read();
