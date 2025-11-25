de0868d0c1500be2f330cbebafcf28a13f9885d0
noah0504789 <58963724+noah0504789@users.noreply.github.com>
2025-11-25 22:30:21 +0900

    [Gold II] Title: 경로 찾기, Time: 256 ms, Memory: 73304 KB -BaekjoonHub

--- 백준/Gold/1513. 경로 찾기/경로 찾기.java
+++ 백준/Gold/1513. 경로 찾기/경로 찾기.java
@@ -4,8 +4,10 @@ import java.util.*;
 public class Main {
     
     private static StringBuilder sb;
-    private static int n, m, c_, p = 1_000_007;
-    private static int[][] stations;
+    private static int n, m, c_, p = 1_000_007, INF = Integer.MAX_VALUE;
+    private static int[][] stations, dir = {
+        {0, 1}, {1, 0}
+    };
     private static long[][][][] dp;
     
     public static void main(String... args) throws IOException {
@@ -17,22 +19,23 @@ public class Main {
         
         stations = new int[n+1][m+1];
         for (int i = 1; i <= c_; i++) stations[readInt()][readInt()] = i;
-        
-        dp = new long[n+1][m+1][c_+1][c_+1];        
-        
+
+        dp = new long[n+1][m+1][c_+1][c_+1];
         int first = stations[1][1];
         if (first == 0) dp[1][1][0][0] = 1;
         else dp[1][1][1][first] = 1;
         
         for (int r = 1; r <= n; r++) {
             for (int c = 1; c <= m; c++) {
-                int cur = stations[r][c];                
+                if (r == 1 && c == 1) continue;
+                
+                int cur = stations[r][c];
                 
                 for (int k = 0; k <= c_; k++) {
                     for (int last = 0; last <= c_; last++) {
                         long up = dp[r-1][c][k][last];
                         long left = dp[r][c-1][k][last];
-                        long v = (up+left) % p;
+                        long v = (up + left) % p;
                         if (v == 0) continue;
                         
                         if (cur == 0) dp[r][c][k][last] = (dp[r][c][k][last] + v) % p;
@@ -44,12 +47,12 @@ public class Main {
         
         for (int k = 0; k <= c_; k++) {
             long sum = 0;
-            for (int last = 0; last <= c_; last++) sum = (sum+dp[n][m][k][last]) % p;
-            sb.append(sum+" ");
-        }
-
+            for (int last = 0; last <= c_; last++) sum = (sum + dp[n][m][k][last]) % p;
+            sb.append(sum).append(" ");    
+        }        
+        
         System.out.print(sb);
-    }
+    }    
 
     public static int readInt() throws IOException {
         int r = 0, c = System.in.read();
