200bd81d3548a22ea778c00f336c112eb9ab7e9e
noah0504789 <58963724+noah0504789@users.noreply.github.com>
2025-11-25 22:17:46 +0900

    [Gold IV] Title: 달려달려, Time: 276 ms, Memory: 56464 KB -BaekjoonHub

--- 백준/Gold/1757. 달려달려/달려달려.java
+++ 백준/Gold/1757. 달려달려/달려달려.java
@@ -4,32 +4,33 @@ import java.util.*;
 public class Main {
     
     private static int n, m, INF = Integer.MAX_VALUE;
-    private static int[] dist;
-    private static long[][] dp; // 거리가 i이고, m이 j일때, n까지 갈수 있는 최대 거리
+    private static int[] speed;
+    private static long[][] dp;
     
     public static void main(String... args) throws IOException {
         n = readInt();
         m = readInt();
-        dist = new int[n];
-        for (int i = 0; i < n; i++) dist[i] = readInt();
+        
+        speed = new int[n];
+        for (int i = 0; i < n; i++) speed[i] = readInt();
         
         dp = new long[n+1][m+1];
-        for (int i = 0; i <= n; i++) Arrays.fill(dp[i], -INF);
+        for (int i = 0; i <= n; i++) Arrays.fill(dp[i], -INF);        
 
         System.out.print(dfs(0, 0));
     }
     
-    private static long dfs(int i, int j) {
-        if (i == n) return j == 0 ? 0 : -INF;
-        if (dp[i][j] != -INF) return dp[i][j];
+    private static long dfs(int i, int t) {
+        if (i == n) return t == 0 ? 0 : -INF;
+        if (dp[i][t] != -INF) return dp[i][t];
         
         long max = -INF;
-        if (j == 0) max = Math.max(max, dfs(i+1, 0));
-        else if (i+j <= n) max = Math.max(max, dfs(i+j, 0)); 
+        if (t == 0) max = Math.max(max, dfs(i+1, 0));
+        else if (i+t <= n) max = Math.max(max, dfs(i+t, 0));
         
-        if (j+1 <= m) max = Math.max(max, dist[i] + dfs(i+1, j+1));
+        if (t+1 <= m) max = Math.max(max, speed[i] + dfs(i+1, t+1));
         
-        return dp[i][j] = max;
+        return dp[i][t] = max;
     }
 
     public static int readInt() throws IOException {
