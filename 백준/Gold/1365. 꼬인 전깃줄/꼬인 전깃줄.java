adbaa4b3a6ec2adbac254e32fdd82282011ecfd6
noah0504789 <58963724+noah0504789@users.noreply.github.com>
2025-11-28 09:13:48 +0900

    [Gold II] Title: 꼬인 전깃줄, Time: 136 ms, Memory: 14968 KB -BaekjoonHub

---
+++ 백준/Gold/1365. 꼬인 전깃줄/꼬인 전깃줄.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+        
+    private static int n, len;
+    private static int[] arr, tails;
+    
+    public static void main(String... args) throws IOException {
+        n = readInt();
+        arr = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = readInt();
+        
+        tails = new int[n];
+        for (int i = 0; i < n; i++) {
+            int x = arr[i];
+            int pos = lowerBound(len, x);
+            tails[pos] = x;
+            if (pos == len) len++;
+        }
+
+        System.out.print(n-len);
+    }
+    
+    private static int lowerBound(int r, int x) {
+        int l = 0;
+        while (l < r) {
+            int m = (l+r)>>>1;
+            if (tails[m]>=x) r = m;
+            else l = m+1;
+        }
+        return l;
+    }
+
+    public static int readInt() throws IOException {
+        int r = 0, c = System.in.read();
+        boolean negative = false;       
+
+        while (c <= ' ') c = System.in.read();
+        if (c == '-') {
+            negative = true;
+            c = System.in.read();
+        }
+        while (c >= '0' && c <= '9') {
+            r *= 10;
+            r += c - '0';
+            c = System.in.read();
+        }
+
+        return negative ? -r : r;
+    }
+}
