410936a40a53ebd0f13f20e4d76ae036e01265bb
noah0504789 <58963724+noah0504789@users.noreply.github.com>
2025-11-30 22:16:21 +0900

    [Gold V] Title: 한글 LCS, Time: 312 ms, Memory: 162260 KB -BaekjoonHub

---
+++ 백준/Gold/15482. 한글 LCS/한글 LCS.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static String a, b;
+    private static int n, m;
+    private static int[][] dp;
+    
+    public static void main(String... args) throws IOException {
+        a = br.readLine();
+        b = br.readLine();
+        
+        n = a.length();
+        m = b.length();
+        
+        dp = new int[n+1][m+1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                String a_ = a.substring(i-1, i);
+                String b_ = b.substring(j-1, j);
+                if (a_.equals(b_)) dp[i][j] = dp[i-1][j-1] + 1;
+                else dp[i][j] = Math.max(dp[i][j-1], dp[i-1][j]);
+            }
+        }
+
+        System.out.print(dp[n][m]);
+    }
+
+    public static int readInt() throws IOException {
+        int r = 0, c = System.in.read();
+        boolean negative = false;       
+
+        while (c <= ' ') c = System.in.read();
+        if (c == '-') {
+            negative = true;
+            c = System.in.read();
+        }
+        while (c >= '0' && c <= '9') {
+            r *= 10;
+            r += c - '0';
+            c = System.in.read();
+        }
+
+        return negative ? -r : r;
+    }
+}
