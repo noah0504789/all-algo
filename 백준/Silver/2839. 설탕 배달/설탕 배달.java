0865af39fab9339b29c8e0a9b132f8e63e05c5b3
noah0504789 <58963724+noah0504789@users.noreply.github.com>
2025-12-01 12:54:53 +0900

    [Silver IV] Title: 설탕 배달, Time: 104 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    
+    private static int n, INF = Integer.MAX_VALUE;
+    private static int[] dp, coins = {3, 5};
+    
+    public static void main(String... args) throws IOException {
+        n = readInt();
+        
+        dp = new int[n+1];
+        Arrays.fill(dp, INF);
+        dp[0] = 0;
+        for (int coin : coins) {
+            for (int i = coin; i <= n; i++) {
+                if (dp[i-coin] != INF) dp[i] = Math.min(dp[i], dp[i-coin]+1);
+            }
+        }
+
+        System.out.print(dp[n] == INF ? -1 : dp[n]);
+    }
+
+    public static int readInt() throws IOException {
+        int r = 0, c = System.in.read();
+        boolean negative = false;       
+
+        while (c <= ' ') c = System.in.read();
+        if (c == '-') {
+            negative = true;
+            c = System.in.read();
+        }
+        while (c >= '0' && c <= '9') {
+            r *= 10;
+            r += c - '0';
+            c = System.in.read();
+        }
+
+        return negative ? -r : r;
+    }
+}
