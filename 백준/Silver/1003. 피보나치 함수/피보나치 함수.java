227bb803a4fb90358515f82d2febc1a6d1bb42fc
noah0504789 <58963724+noah0504789@users.noreply.github.com>
2025-12-01 13:46:08 +0900

    [Silver III] Title: 피보나치 함수, Time: 120 ms, Memory: 15984 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -2,60 +2,60 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    private static BufferedWriter bw;
-    private static int[][] dp;
-    private static int tc, n;
-
-    public static void main(String... args) throws IOException {
-        bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        tc = readInt();
-
-        dp = new int[41][2];
-
-        for (int i = 0; i < tc; i++) {
-            n = readInt();
-
-            fibDP(n);
-
-            bw.write(dp[n][0] + " " + dp[n][1]);
-            bw.write("\n");
-        }
-
-        bw.flush();
-    }
-
-    private static int[] fibDP(int n) {
-        if (n == 0) {
-            dp[0][0] = 1;
-            return dp[0];
-        }
-
-        if (n == 1) {
-            dp[1][1] = 1;
-            return dp[1];
-        }
-
-        if (dp[n][0] > 0 && dp[n][1] > 0) return dp[n];
         
-        int[] one = fibDP(n-1), two = fibDP(n-2);
+    private static StringBuilder sb = new StringBuilder();
+    private static int tc, maxN;
+    private static int[] tArr;
+    private static boolean[] vis;
+    private static int[][] dp;
+    
+    public static void main(String... args) throws IOException {
+        tc = readInt();
+        tArr = new int[tc];
+        for (int i = 0; i < tc; i++) {
+            tArr[i] = readInt();
+            maxN = Math.max(maxN, tArr[i]);
+        }                
+        
+        dp = new int[Math.max(maxN, 1)+1][2];
+        dp[0][0] = dp[1][1] = 1;
+        
+        vis = new boolean[Math.max(maxN, 1)+1];
+        vis[0] = vis[1] = true;
+        
+        fib(maxN);
+        for (int t : tArr) sb.append(dp[t][0] + " " + dp[t][1]).append("\n");
 
-        dp[n][0] = one[0] + two[0];
-        dp[n][1] = one[1] + two[1];
-
-        return dp[n];
+        System.out.print(sb);
+    }
+    
+    private static int[] fib(int n) {
+        if (n < 0) return new int[]{0, 0};
+        if (vis[n]) return dp[n];
+        vis[n] = true;
+        
+        return dp[n] = add(fib(n-1), fib(n-2));
+    }
+    
+    private static int[] add(int[] a, int[] b) {        
+        return new int[]{a[0] + b[0], a[1] + b[1]};
     }
 
     public static int readInt() throws IOException {
         int r = 0, c = System.in.read();
+        boolean negative = false;       
 
         while (c <= ' ') c = System.in.read();
+        if (c == '-') {
+            negative = true;
+            c = System.in.read();
+        }
         while (c >= '0' && c <= '9') {
             r *= 10;
             r += c - '0';
             c = System.in.read();
         }
 
-        return r;
+        return negative ? -r : r;
     }
 }
