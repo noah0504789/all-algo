35eaee5bbe11692275000c213fd17dcf57787ed5
noah0504789 <58963724+noah0504789@users.noreply.github.com>
2025-12-02 20:02:05 +0900

    [Silver I] Title: 1, 2, 3 더하기 5, Time: 132 ms, Memory: 21228 KB -BaekjoonHub

--- 백준/Silver/15990. 1， 2， 3 더하기 5/1， 2， 3 더하기 5.java
+++ 백준/Silver/15990. 1， 2， 3 더하기 5/1， 2， 3 더하기 5.java
@@ -5,39 +5,38 @@ public class Main {
     
     private static StringBuilder sb;
     private static int tc, n, maxN, p = 1_000_000_009;
-    private static int[] arr;
+    private static int[] qs;
     private static long[][] dp;
     
     public static void main(String... args) throws IOException {
         sb = new StringBuilder();
         
         tc = readInt();
-        arr = new int[tc];
+        
+        qs = new int[tc];
         for (int i = 0; i < tc; i++) {
-            arr[i] = readInt();
-            maxN = Math.max(maxN, arr[i]);
+            qs[i] = readInt();
+            if (qs[i] > maxN) maxN = qs[i];
         }
         
-        dp=new long[Math.max(3, maxN)+1][4];
-        dp[1][1]=1;
-        dp[2][2]=1;
-        dp[3][1]=1;
-        dp[3][2]=1;
-        dp[3][3]=1;
-        
-        for (int i = 4; i <= maxN; i++) {
-            dp[i][1] = (dp[i-1][2] + dp[i-1][3])%p;
-            dp[i][2] = (dp[i-2][1] + dp[i-2][3])%p;
-            dp[i][3] = (dp[i-3][1] + dp[i-3][2])%p;
+        dp = new long[Math.max(3, maxN)+1][4];
+        dp[1][1] = 1L;
+        dp[2][2] = 1L;            
+        dp[3][1] = dp[3][2] = dp[3][3] = 1L;
+
+        for (int i = 4; i <= maxN; i++) {             
+            dp[i][1] = (dp[i-1][2] + dp[i-1][3]) % p;
+            dp[i][2] = (dp[i-2][1] + dp[i-2][3]) % p;
+            dp[i][3] = (dp[i-3][1] + dp[i-3][2]) % p;
         }
         
-        for (int i = 0; i < tc; i++) sb.append(sum(arr[i])+"\n");
+        for (int i = 0; i < tc; i++) {             
+            n = qs[i];
+            
+            sb.append((dp[n][1] + dp[n][2] + dp[n][3]) % p).append("\n");
+        }
 
         System.out.print(sb);
-    }
-    
-    private static long sum(int i) {
-        return (dp[i][1]+dp[i][2]+dp[i][3])%p;
     }
 
     public static int readInt() throws IOException {
