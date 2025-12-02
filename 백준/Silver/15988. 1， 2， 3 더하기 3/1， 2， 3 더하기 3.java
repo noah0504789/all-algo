77826e10c676e7edec7acc9abbeeb07e20d8291d
noah0504789 <58963724+noah0504789@users.noreply.github.com>
2025-12-02 19:27:48 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 148 ms, Memory: 22344 KB -BaekjoonHub

---
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    
+    private static StringBuilder sb = new StringBuilder();
+    private static int tc, maxN, p = 1_000_000_009;
+    private static int[] tArr;
+    private static long[] dp;
+    
+    public static void main(String... args) throws IOException {
+        tc = readInt();
+        tArr = new int[tc];
+        for (int i = 0; i < tc; i++) {
+            tArr[i] = readInt();
+            maxN = Math.max(maxN, tArr[i]);
+        }
+        
+        dp = new long[Math.max(3, maxN)+1];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+        for (int i = 4; i <= maxN; i++) dp[i] = (dp[i-1] + dp[i-2] + dp[i-3]) % p;
+        
+        for (int t : tArr) sb.append(dp[t]+"\n");
+        
+        System.out.print(sb);
+    }
+
+    public static int readInt() throws IOException {
+        int r = 0, c = System.in.read();
+        boolean negative = false;       
+
+        while (c <= ' ') c = System.in.read();
+        if (c == '-') {
+            negative = true;
+            c = System.in.read();
+        }
+        while (c >= '0' && c <= '9') {
+            r *= 10;
+            r += c - '0';
+            c = System.in.read();
+        }
+
+        return negative ? -r : r;
+    }
+}
