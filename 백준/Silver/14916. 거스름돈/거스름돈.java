041aaf71b81c026062dc6ac1478c96b92b1b37e9
noah0504789 <58963724+noah0504789@users.noreply.github.com>
2025-12-02 18:03:29 +0900

    [Silver V] Title: 거스름돈, Time: 112 ms, Memory: 14424 KB -BaekjoonHub

--- 백준/Silver/14916. 거스름돈/거스름돈.java
+++ 백준/Silver/14916. 거스름돈/거스름돈.java
@@ -1,42 +1,40 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    private static int n;
-
+    
+    private static int n, INF = Integer.MAX_VALUE;
+    private static int[] dp, coins = {2, 5};
+    
     public static void main(String... args) throws IOException {
         n = readInt();
-        
-        int cnt = 0;
-        
-        while (true) {
-            if (n % 5 == 0) {
-                cnt += n / 5;                
-                break;
-            } else {
-                n -= 2;
-                cnt++;
-            }
-            
-            if (n < 0) {
-                cnt = -1;
-                break;
+        dp = new int[n+1];
+        Arrays.fill(dp, INF);
+        dp[0] = 0;
+        for (int coin : coins) {
+            for (int i = coin; i <= n; i++) {
+                if (dp[i-coin] != INF) dp[i] = Math.min(dp[i], dp[i-coin] + 1);
             }
         }
-
-        System.out.print(cnt);
+        
+        System.out.print(dp[n] == INF ? -1 : dp[n]);
     }
 
-    private static int readInt() throws IOException {
-        int result = 0, c = System.in.read();
+    public static int readInt() throws IOException {
+        int r = 0, c = System.in.read();
+        boolean negative = false;       
 
         while (c <= ' ') c = System.in.read();
-
+        if (c == '-') {
+            negative = true;
+            c = System.in.read();
+        }
         while (c >= '0' && c <= '9') {
-            result *= 10;
-            result += c - '0';
+            r *= 10;
+            r += c - '0';
             c = System.in.read();
         }
 
-        return result;
+        return negative ? -r : r;
     }
 }
