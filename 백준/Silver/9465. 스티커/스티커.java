623c76e5acefe4742628812565bb2ab1b2d39e79
noah0504789 <58963724+noah0504789@users.noreply.github.com>
2025-12-02 14:15:25 +0900

    [Silver I] Title: 스티커, Time: 276 ms, Memory: 33128 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.java
+++ 백준/Silver/9465. 스티커/스티커.java
@@ -5,50 +5,27 @@ public class Main {
         
     private static StringBuilder sb = new StringBuilder();
     private static int tc, n;
-    private static int[][] arr;
-    private static long[][][] dp;
+    private static int[][] arr, dp;
     
     public static void main(String... args) throws IOException {
         tc = readInt();
         while(tc-- > 0) {
             n = readInt();
-            arr = new int[2][n];
-            for (int i = 0; i < n; i++) arr[0][i] = readInt();            
-            for (int i = 0; i < n; i++) arr[1][i] = readInt();
+            arr = new int[2][n+1];
+            for (int i = 1; i <= n; i++) arr[0][i] = readInt();            
+            for (int i = 1; i <= n; i++) arr[1][i] = readInt();
             
-            if (n == 1) {
-                //sb.append(Math.max(arr[0][0], arr[0][1])).append("\n");    
-                //continue;
+            dp = new int[3][n+1];
+            for (int i = 1; i <= n; i++) {
+                dp[0][i] = Math.max(dp[0][i-1], Math.max(dp[1][i-1], dp[2][i-1]));
+                dp[1][i] = Math.max(dp[0][i-1], dp[2][i-1]) + arr[0][i];
+                dp[2][i] = Math.max(dp[0][i-1], dp[1][i-1]) + arr[1][i];
             }
             
-            if (n == 2) {
-                //sb.append(Math.max(arr[0][0] + arr[1][1], arr[0][1] + arr[1][0])).append("\n");    
-                //continue;
-            }
-            
-            dp = new long[2][n][2];
-            for (int i = 0; i < 2; i++) {
-                for (int j = 0; j < n; j++) Arrays.fill(dp[i][j], -1);
-            }
-            
-            sb.append(dfs(0, 0, 1)).append("\n");
+            sb.append(Math.max(dp[0][n], Math.max(dp[1][n], dp[2][n]))).append("\n");
         }
         
         System.out.print(sb);
-    }
-    
-    private static long dfs(int r, int c, int verOK) {
-        if (c == n) return 0;
-        if (dp[r][c][verOK] != -1) return dp[r][c][verOK];
-        
-        long max = 0;
-        max = Math.max(max, dfs(r, c+1, 1));
-        // 현재 점에서 먹기
-        max = Math.max(max, arr[r][c] + dfs((r+1)%2, c+1, 0));
-        // 아래 점에서 먹기
-        if (verOK == 1) max = Math.max(max, arr[(r+1)%2][c] + dfs(r, c+1, 0));
-        
-        return dp[r][c][verOK] = max;                       
     }
 
     public static int readInt() throws IOException {
