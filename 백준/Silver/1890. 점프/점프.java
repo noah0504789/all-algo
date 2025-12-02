4452a384f5d7c676d160abc56f6ffd039b54eb9d
noah0504789 <58963724+noah0504789@users.noreply.github.com>
2025-12-02 18:00:29 +0900

    [Silver I] Title: 점프, Time: 104 ms, Memory: 14104 KB -BaekjoonHub

--- 백준/Silver/1890. 점프/점프.java
+++ 백준/Silver/1890. 점프/점프.java
@@ -2,48 +2,48 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    private static BufferedWriter bw;
-
+    
+    private static int n;
+    private static int[][] arr;
     private static long[][] dp;
-    private static int[][] board;
-    private static int n, jump;
-
+    
     public static void main(String... args) throws IOException {
-        bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         n = readInt();
-
-        board = new int[n+1][n+1];
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) board[i][j] = readInt();
+        arr = new int[n+1][n+1];
+        for (int r = 1; r <= n; r++) {
+            for (int c = 1; c <= n; c++) arr[r][c] = readInt();
         }
-
-        dp = new long[n+1][n+1];
-        dp[1][1] = 1;
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                jump = board[i][j];
-                if (jump == 0) break;
-
-                if (i+jump <= n) dp[i+jump][j] += dp[i][j];
-                if (j+jump <= n) dp[i][j+jump] += dp[i][j];
-            }
-        }
-
-        bw.write(dp[n][n]+"");
-        bw.flush();
+        
+        dp = new long[n+1][n+1];       
+        for (int i = 0; i <= n; i++) Arrays.fill(dp[i], -1);
+        
+        System.out.print(dfs(1, 1));
+    }
+    
+    private static long dfs(int r, int c) {
+        if (r == n && c == n) return 1;
+        if (dp[r][c] != -1) return dp[r][c];
+        
+        int v = arr[r][c];
+        
+        return dp[r][c] = v > 0 ? (r+v<=n ? dfs(r+v, c) : 0) + (c+v<=n ? dfs(r, c+v) : 0) : 0;
     }
 
     public static int readInt() throws IOException {
         int r = 0, c = System.in.read();
+        boolean negative = false;       
 
         while (c <= ' ') c = System.in.read();
+        if (c == '-') {
+            negative = true;
+            c = System.in.read();
+        }
         while (c >= '0' && c <= '9') {
             r *= 10;
             r += c - '0';
             c = System.in.read();
         }
 
-        return r;
+        return negative ? -r : r;
     }
 }
