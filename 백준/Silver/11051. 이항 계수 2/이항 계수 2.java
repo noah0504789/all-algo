cc0319398bbc92740f08ae7fc95975625445f149
noah0504789 <58963724+noah0504789@users.noreply.github.com>
2025-12-02 16:51:30 +0900

    [Silver II] Title: 이항 계수 2, Time: 124 ms, Memory: 21916 KB -BaekjoonHub

--- 백준/Silver/11051. 이항 계수 2/이항 계수 2.java
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.java
@@ -2,38 +2,39 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    private static final int MOD = 10_007;
     
-    private static int[][] dp;
-    private static int n, k;
-
+    private static int n, k, p = 10_007;
+    private static long[][] comb;
+    
     public static void main(String... args) throws IOException {
         n = readInt();
         k = readInt();
         
-        dp = new int[n+1][k+1];
-
-        System.out.print(comb(n, k));
-    }
-    
-    public static int comb(int n, int r) {
-        if (n == 0 || r == 0 || n == r) return 1;
-        if (r == 1) return n;
-        if (dp[n][r] > 0) return dp[n][r];
-        
-        return dp[n][r] = (comb(n-1, r) + comb(n-1, r-1)) % MOD;
+        comb = new long[n+1][n+1];
+        comb[0][0] = 1;
+        for (int i = 1; i <= n; i++) {
+            comb[i][0] = comb[i][i] = 1;
+            for (int j = 1; j <= k; j++) comb[i][j] = (comb[i-1][j-1] + comb[i-1][j]) % p;
+        }
+                
+        System.out.print(comb[n][k] % p);
     }
 
     public static int readInt() throws IOException {
         int r = 0, c = System.in.read();
+        boolean negative = false;       
 
         while (c <= ' ') c = System.in.read();
+        if (c == '-') {
+            negative = true;
+            c = System.in.read();
+        }
         while (c >= '0' && c <= '9') {
             r *= 10;
             r += c - '0';
             c = System.in.read();
         }
 
-        return r;
+        return negative ? -r : r;
     }
 }
