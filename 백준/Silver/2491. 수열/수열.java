b28d7ad17f65764a73f7e7d1ccb18d17f1da6a1b
noah0504789 <58963724+noah0504789@users.noreply.github.com>
2025-12-03 14:54:17 +0900

    [Silver IV] Title: 수열, Time: 132 ms, Memory: 15708 KB -BaekjoonHub

--- 백준/Silver/2491. 수열/수열.java
+++ 백준/Silver/2491. 수열/수열.java
@@ -3,26 +3,38 @@ import java.util.*;
 
 public class Main {
     
-    private static int n, inc, dec, ans;
-    private static int[] arr;
+    private static int n, len, ans;
+    private static int[] arr, dp;
     
     public static void main(String... args) throws IOException {
         n = readInt();
-        
         arr = new int[n];
         for (int i = 0; i < n; i++) arr[i] = readInt();
         
-        inc = 1;
-        dec = 1;
         ans = 1;
+        dp = new int[n];
+        dp[0] = 1;
         for (int i = 1; i < n; i++) {
-            inc = arr[i-1] <= arr[i] ? inc+1 : 1;
-            dec = arr[i-1] >= arr[i] ? dec+1 : 1;
-            ans = Math.max(ans, Math.max(inc, dec));
+            if (arr[i] >= arr[i-1]) {
+                dp[i] = dp[i-1] + 1;                
+            }
+            else dp[i] = 1;
+            ans = Math.max(ans, dp[i]);
+        }
+        
+        dp = new int[n];
+        dp[0] = 1;
+        for (int i = 1; i < n; i++) {
+            if (arr[i] <= arr[i-1]) {
+                dp[i] = dp[i-1] + 1;
+                
+            }
+            else dp[i] = 1;            
+            ans = Math.max(ans, dp[i]);
         }
         
         System.out.print(ans);
-    }    
+    }
 
     public static int readInt() throws IOException {
         int r = 0, c = System.in.read();
