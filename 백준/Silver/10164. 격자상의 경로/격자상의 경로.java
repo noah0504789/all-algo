fc5572f160e009ab4526855c0e426110427ae86f
noah0504789 <58963724+noah0504789@users.noreply.github.com>
2025-12-03 16:16:34 +0900

    [Silver II] Title: 격자상의 경로, Time: 108 ms, Memory: 14252 KB, Score: 32 point -BaekjoonHub

--- 백준/Silver/10164. 격자상의 경로/격자상의 경로.java
+++ 백준/Silver/10164. 격자상의 경로/격자상의 경로.java
@@ -3,63 +3,45 @@ import java.util.*;
 
 public class Main {
     
-    private static int n, m, k, r, c;
-    private static long[][] dp1, dp2;
+    private static int n, m, k, a, b;
+    private static int[][] dp;
     
     public static void main(String... args) throws IOException {
         n = readInt();
         m = readInt();
-        k = readInt()-1;
-        if (k == -1) {
-            dp1 = new long[n][m];
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < m; j++) {
-                    if (i == 0 && j == 0) {
-                        dp1[i][j] = 1;
-                        continue;
-                    }
-                    long l = i-1 >= 0 ? dp1[i-1][j] : 0;
-                    long r = j-1 >= 0 ? dp1[i][j-1] : 0;
-                    dp1[i][j] = l+r;
+        k = readInt();
+        if (k == 0) {
+            dp = new int[n+1][m+1];        
+            for (int r = 1; r <= n; r++) {
+                for (int c = 1; c <= m; c++) {
+                    if (r == 1 && c == 1) {dp[r][c] = 1; continue;}
+                    dp[r][c] = dp[r-1][c] + dp[r][c-1];
                 }
             }
             
-            System.out.print(dp1[n-1][m-1]);
-            return;
-        }
+            System.out.print(dp[n][m]);
+            return;  
+        } 
         
-        r = k / m;
-        c = k % m;
-        
-        dp1 = new long[r+1][c+1];
-        for (int i = 0; i <= r; i++) {
-            for (int j = 0; j <= c; j++) {
-                if (i == 0 && j == 0) {
-                    dp1[i][j] = 1;
-                    continue;
-                }
-                long l = i-1 >= 0 ? dp1[i-1][j] : 0;
-                long r = j-1 >= 0 ? dp1[i][j-1] : 0;
-                dp1[i][j] = l+r;
+        dp = new int[n+1][m+1];        
+        for (int r = 1; r <= (k/m)+1; r++) {
+            for (int c = 1; c <= k%m; c++) {
+                if (r == 1 && c == 1) {dp[r][c] = 1; continue;}
+                dp[r][c] = dp[r-1][c] + dp[r][c-1];
             }
         }
         
-        int h = n-r;
-        int w = m-c;
-        dp2 = new long[h][w];
-        for (int i = 0; i < h; i++) {
-            for (int j = 0; j < w; j++) {
-                if (i == 0 && j == 0) {
-                    dp2[i][j] = 1;
-                    continue;
-                }
-                long l = i-1 >= 0 ? dp2[i-1][j] : 0;
-                long r = j-1 >= 0 ? dp2[i][j-1] : 0;
-                dp2[i][j] = l+r;
+        a = dp[(k/m)+1][k%m];
+        
+        dp = new int[n+1][m+1];
+        for (int r = (k/m)+1; r <= n; r++) {
+            for (int c = k%m; c <= m; c++) {
+                if (r == (k/m)+1 && c == k%m) {dp[r][c] = 1; continue;}
+                dp[r][c] = dp[r-1][c] + dp[r][c-1];
             }
         }
-
-        System.out.print(dp1[r][c] * dp2[h-1][w-1]);
+        
+        System.out.print(a * dp[n][m]);
     }
 
     public static int readInt() throws IOException {
